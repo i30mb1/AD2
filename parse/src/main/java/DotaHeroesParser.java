44765b8f356c018b3aff742a5385c694b1e830a6
i30mb1 <fate.i30mb1@gmail.com>
2019-11-10 21:27:55 +0300

    94

--- parse/src/main/java/DotaHeroesParser.java
+++ parse/src/main/java/DotaHeroesParser.java
@@ -30,7 +30,7 @@ class DotaHeroesParser {
         int size = loadHeroesFromFile().size();
         for (String hero : loadHeroesFromFile()) {
             String counter = String.format(Locale.US, "% d/%d ", ++count, size);
-            loadResponses(hero, counter);
+//            loadResponses(hero, counter);
 //            loadSpellsAndDescription(hero, counter);
         }
 
@@ -1011,7 +1011,11 @@ class DotaHeroesParser {
                 JSONObject jsonObject = new JSONObject();//создаём обьект для героя
                 JSONArray jsonArrayAbilities = new JSONArray();//ряды для способностей
                 JSONObject jsonObjectAbility = new JSONObject();//обьект для одной способности
-                jsonObject.put("desc", doc.getElementsByClass("mw-parser-output").get(0).child(2).text()); //кладём описание героя
+                if (language.equals("eng")) {
+                    jsonObject.put("desc", doc.getElementsByClass("mw-parser-output").get(0).child(2).text()); //кладём описание героя
+                } else {
+                    jsonObject.put("desc", doc.getElementsByClass("mw-parser-output").get(1).child(2).text()); //кладём описание героя
+                }
                 Elements children = doc.getElementById("mw-content-text").child(0).children();
                 boolean findBio = false;
                 boolean findAbilities = false;
