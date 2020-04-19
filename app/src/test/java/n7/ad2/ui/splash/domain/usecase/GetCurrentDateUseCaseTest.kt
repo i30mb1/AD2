4601a4e074a210e7988fafb81db30463ab942d33
i30mb1 <fate.i30mb1@gmail.com>
2020-04-19 13:48:06 +0300

    now test seems reasonable

--- app/src/test/java/n7/ad2/ui/splash/domain/usecase/GetCurrentDateUseCaseTest.kt
+++ app/src/test/java/n7/ad2/ui/splash/domain/usecase/GetCurrentDateUseCaseTest.kt
@@ -3,7 +3,6 @@ package n7.ad2.ui.splash.domain.usecase
 import androidx.test.filters.FlakyTest
 import androidx.test.filters.SmallTest
 import com.google.common.truth.Truth.assertThat
-import com.nhaarman.mockitokotlin2.mock
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import n7.ad2.CoroutineTestRule
 import n7.ad2.runBlockingTest
@@ -16,33 +15,44 @@ import java.util.*
 @FlakyTest // we should not test Data classes that why we mocked Calendar
 class GetCurrentDateUseCaseTest {
 
+    companion object {
+        const val YEAR = 0
+        const val MONTH = 0
+        const val DAY = 1
+    }
+
     @get:Rule
     val coroutineTestRule = CoroutineTestRule()
 
-    val getCurrentDateUseCase = GetCurrentDateInYearUseCase(coroutineTestRule.testDispatcher, mock())
+    val testDate = Calendar.getInstance().apply {
+        set(YEAR, MONTH, DAY)
+    }
+
+    val getCurrentDateInYearUseCase = GetCurrentDateInYearUseCase(coroutineTestRule.testDispatcher, testDate)
 
     @Test
     fun `current day not null`() = coroutineTestRule.runBlockingTest {
-        val day = getCurrentDateUseCase(Date(0))
-        assertThat(day).isNotNull()
+        getCurrentDateInYearUseCase().let {
+            assertThat(it).isNotNull()
+        }
     }
 
     @Test
     fun `current day in diapason 0 to 366`() = coroutineTestRule.runBlockingTest {
-        val day = getCurrentDateUseCase(Date(Long.MAX_VALUE))
-        assertThat(day).isIn(0..366)
+        getCurrentDateInYearUseCase(Date(Long.MAX_VALUE)).let {
+            assertThat(it).isIn(0..366)
+        }
 
-        getCurrentDateUseCase(Date(Long.MAX_VALUE))
-        assertThat(day).isIn(0..366)
+        getCurrentDateInYearUseCase(Date(Long.MIN_VALUE)).let {
+            assertThat(it).isIn(0..366)
+        }
     }
 
     @Test
-    fun `current day with expected result 1`() = coroutineTestRule.runBlockingTest {
-        val calendar = Calendar.getInstance()
-        calendar.clear()
-        val expectedDay = 1
-        val actualDay = getCurrentDateUseCase(calendar.time)
-        assertThat(actualDay).isEqualTo(expectedDay)
+    fun `current day with expected result`() = coroutineTestRule.runBlockingTest {
+        getCurrentDateInYearUseCase().let {
+            assertThat(it).isEqualTo(DAY)
+        }
     }
 
 }
