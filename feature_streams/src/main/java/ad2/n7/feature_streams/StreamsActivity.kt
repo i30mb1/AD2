05cb5657080f13b8ff804d61685c04c8a6d89d86
i30mb1 <fate.i30mb1@gmail.com>
2020-04-27 18:34:53 +0300

    now instant enabled dynamic feature works!

--- feature_streams/src/main/java/ad2/n7/feature_streams/StreamsActivity.kt
+++ feature_streams/src/main/java/ad2/n7/feature_streams/StreamsActivity.kt
@@ -1,12 +1,22 @@
 package ad2.n7.feature_streams
 
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
 class StreamsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(n7.ad2.R.style.AD2Theme_White)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_streams)
+
+        val action: String? = intent?.action
+        val data: Uri? = intent?.data
+
+        Log.d("N7",action.toString())
+        Log.d("N7",data.toString())
+
     }
 }
