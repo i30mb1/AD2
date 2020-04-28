3c776814a4e4ddcfcbd82e248d8b55f31b985ddf
i30mb1 <fate.i30mb1@gmail.com>
2020-04-28 17:24:47 +0300

    add base dao for all daos in project

--- app/src/main/java/n7/ad2/data/source/local/db/HeroesDao.kt
+++ app/src/main/java/n7/ad2/data/source/local/db/HeroesDao.kt
@@ -3,10 +3,11 @@ package n7.ad2.data.source.local.db
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Query
+import n7.ad2.base.BaseDao
 import n7.ad2.data.source.local.model.LocalHero
 
 @Dao
-interface HeroesDao {
+interface HeroesDao : BaseDao<LocalHero> {
 
     @Query("SELECT * FROM LocalHeroes")
     fun getAll(): LiveData<List<LocalHero>>
