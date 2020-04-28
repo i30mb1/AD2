644876389d159a886f809a4d2513c098e0545e2a
i30mb1 <fate.i30mb1@gmail.com>
2020-04-28 15:57:14 +0300

    run UseCase on backgroundThread

--- app/src/main/java/n7/ad2/ui/splash/domain/usecase/GetAssetsHeroesFromJsonUseCase.kt
+++ app/src/main/java/n7/ad2/ui/splash/domain/usecase/GetAssetsHeroesFromJsonUseCase.kt
@@ -2,6 +2,7 @@ package n7.ad2.ui.splash.domain.usecase
 
 import com.squareup.moshi.Moshi
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
 import n7.ad2.data.source.local.model.AssetsHero
 import n7.ad2.data.source.local.model.AssetsHeroList
 import java.io.FileNotFoundException
@@ -13,11 +14,11 @@ class GetAssetsHeroesFromJsonUseCase @Inject constructor(
         private val ioDispatcher: CoroutineDispatcher,
         private val moshi: Moshi
 ) {
-    operator fun invoke(jsonString: String): List<AssetsHero> {
+    suspend operator fun invoke(jsonString: String): List<AssetsHero> = withContext(ioDispatcher) {
         if (jsonString.isEmpty()) throw FileNotFoundException("File empty or not exist")
 
         val result = moshi.adapter(AssetsHeroList::class.java).fromJson(jsonString)!!
 
-        return result.heroes
+        result.heroes
     }
 }
