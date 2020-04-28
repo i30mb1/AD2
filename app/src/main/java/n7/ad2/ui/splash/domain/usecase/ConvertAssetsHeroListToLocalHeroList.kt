91e8abb37ba2a88d08af40d2358ef3bc9c29c7e0
i30mb1 <fate.i30mb1@gmail.com>
2020-04-28 17:40:02 +0300

    add UseCase in dagger graph

--- app/src/main/java/n7/ad2/ui/splash/domain/usecase/ConvertAssetsHeroListToLocalHeroList.kt
+++ app/src/main/java/n7/ad2/ui/splash/domain/usecase/ConvertAssetsHeroListToLocalHeroList.kt
@@ -3,12 +3,13 @@ package n7.ad2.ui.splash.domain.usecase
 import n7.ad2.data.source.local.model.AssetsHero
 import n7.ad2.data.source.local.model.LocalHero
 import n7.ad2.ui.splash.domain.adapter.toLocalHero
+import javax.inject.Inject
 
-class ConvertAssetsHeroListToLocalHeroList {
+class ConvertAssetsHeroListToLocalHeroList @Inject constructor() {
 
     operator fun invoke(assetsHeroList: List<AssetsHero>): List<LocalHero> {
         return assetsHeroList.map {
             it.toLocalHero()
-    }
+        }
     }
 }
