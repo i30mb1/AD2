8f302defe205f8523619386fb3848b42cffdffc4
i30mb1 <fate.i30mb1@gmail.com>
2020-04-28 14:51:17 +0300

    apply lint rules

--- app/src/test/java/n7/ad2/ui/splash/domain/usecase/GetLocalHeroesFromJsonUseCaseTest.kt
+++ app/src/test/java/n7/ad2/ui/splash/domain/usecase/GetLocalHeroesFromJsonUseCaseTest.kt
@@ -1,10 +1,8 @@
 package n7.ad2.ui.splash.domain.usecase
 
-import com.nhaarman.mockitokotlin2.mock
 import com.squareup.moshi.Moshi
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import n7.ad2.CoroutineTestRule
-import n7.ad2.data.source.local.model.LocalHero
 import n7.ad2.runBlockingTest
 import org.junit.Rule
 import org.junit.Test
@@ -16,7 +14,7 @@ class GetLocalHeroesFromJsonUseCaseTest {
     @get:Rule
     val coroutineTestRule = CoroutineTestRule()
 
-    val moshi = Moshi.Builder().build()
+    private val moshi = Moshi.Builder().build()
 
     val getLocalHeroesFromFileUseCase = GetLocalHeroesFromJsonUseCase(coroutineTestRule.testDispatcher, moshi)
 
@@ -24,7 +22,7 @@ class GetLocalHeroesFromJsonUseCaseTest {
     fun `if file empty throw error`() = coroutineTestRule.runBlockingTest {
         val jsonString = ""
 
-        val list: List<LocalHero> = getLocalHeroesFromFileUseCase(jsonString)
+        getLocalHeroesFromFileUseCase(jsonString)
     }
 
 }
