77d8567b388b537cb05a9287507cd3712e66c309
i30mb1 <fate.i30mb1@gmail.com>
2020-04-28 17:39:24 +0300

    write class that suit for test

---
+++ app/src/main/java/n7/ad2/ui/splash/domain/usecase/ConvertAssetsHeroListToLocalHeroList.kt
@@ -0,0 +1,14 @@
+package n7.ad2.ui.splash.domain.usecase
+
+import n7.ad2.data.source.local.model.AssetsHero
+import n7.ad2.data.source.local.model.LocalHero
+import n7.ad2.ui.splash.domain.adapter.toLocalHero
+
+class ConvertAssetsHeroListToLocalHeroList {
+
+    operator fun invoke(assetsHeroList: List<AssetsHero>): List<LocalHero> {
+        return assetsHeroList.map {
+            it.toLocalHero()
+    }
+    }
+}
