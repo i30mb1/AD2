a4c9fced9acaaba39fb8fcacc67d758b4f7eafb6
i30mb1 <fate.i30mb1@gmail.com>
2020-04-28 13:38:32 +0300

    provide dependency to my DatabaseWorker

--- app/src/main/java/n7/ad2/di/ApplicationComponent.kt
+++ app/src/main/java/n7/ad2/di/ApplicationComponent.kt
@@ -3,7 +3,9 @@ package n7.ad2.di
 import android.app.Application
 import dagger.BindsInstance
 import dagger.Component
+import n7.ad2.data.source.local.db.AppDatabase
 import n7.ad2.ui.splash.SplashViewModel
+import n7.ad2.workers.DatabaseWorker
 import javax.inject.Singleton
 
 @Singleton
@@ -19,6 +21,8 @@ interface ApplicationComponent {
 
         fun create(@BindsInstance applicationContext: Application): ApplicationComponent
     }
+
+    fun inject(databaseWorker: DatabaseWorker)
 
     val splashViewModel: SplashViewModel
 }
