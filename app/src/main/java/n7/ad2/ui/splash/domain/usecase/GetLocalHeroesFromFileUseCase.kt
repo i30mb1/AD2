82e39e05f868773be262857f9e93c01a5dc6ea5b
i30mb1 <fate.i30mb1@gmail.com>
2020-04-28 14:50:00 +0300

    make a UseCase that pass Test

--- app/src/main/java/n7/ad2/ui/splash/domain/usecase/GetLocalHeroesFromFileUseCase.kt
+++
@@ -1,8 +0,0 @@
-package n7.ad2.ui.splash.domain.usecase
-
-import kotlinx.coroutines.CoroutineDispatcher
-import javax.inject.Inject
-
-class GetLocalHeroesFromFileUseCase @Inject constructor(
-        private val ioDispatcher: CoroutineDispatcher
-)
---
+++ app/src/main/java/n7/ad2/ui/splash/domain/usecase/GetLocalHeroesFromJsonUseCase.kt
@@ -0,0 +1,21 @@
+package n7.ad2.ui.splash.domain.usecase
+
+import android.app.Application
+import com.squareup.moshi.Moshi
+import kotlinx.coroutines.CoroutineDispatcher
+import n7.ad2.data.source.local.model.LocalHero
+import java.io.FileNotFoundException
+import javax.inject.Inject
+
+const val HEROES_DATA_FILENAME = "heroesNew.json"
+
+class GetLocalHeroesFromJsonUseCase @Inject constructor(
+        private val ioDispatcher: CoroutineDispatcher,
+        private val moshi: Moshi
+) {
+    operator fun invoke(jsonString: String): List<LocalHero> {
+        if(jsonString.isEmpty()) throw FileNotFoundException("File empty or not exist")
+
+        return emptyList()
+    }
+}
