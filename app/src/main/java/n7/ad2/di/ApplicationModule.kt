eb5a463daf8f8c2320295607384fb3b26aeeb778
i30mb1 <fate.i30mb1@gmail.com>
2020-04-28 12:52:31 +0300

    create dependency in Dagger graph

--- app/src/main/java/n7/ad2/di/ApplicationModule.kt
+++ app/src/main/java/n7/ad2/di/ApplicationModule.kt
@@ -3,15 +3,29 @@ package n7.ad2.di
 import android.app.Application
 import android.content.SharedPreferences
 import androidx.preference.PreferenceManager
+import androidx.room.Room
 import dagger.Module
 import dagger.Provides
 import dagger.Reusable
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import n7.ad2.data.source.local.db.AppDatabase
 import java.util.*
 
 @Module
 object ApplicationModule {
+
+    @Reusable
+    @Provides
+    fun provideDatabase(application: Application): AppDatabase {
+        return Room.databaseBuilder(
+                application,
+                AppDatabase::class.java,
+                AppDatabase.DB_NAME
+        )
+                .fallbackToDestructiveMigration()
+                .build()
+    }
 
     @Reusable
     @Provides
