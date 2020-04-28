10154a30bb03f4312beaf1bb28f90c0747c041eb
i30mb1 <fate.i30mb1@gmail.com>
2020-04-28 17:28:55 +0300

    add adapter from AssetsHero to LocalHero

---
+++ app/src/main/java/n7/ad2/ui/splash/domain/adapter/HeroAdapter.kt
@@ -0,0 +1,12 @@
+package n7.ad2.ui.splash.domain.adapter
+
+import n7.ad2.data.source.local.model.AssetsHero
+import n7.ad2.data.source.local.model.LocalHero
+
+fun AssetsHero.toLocalHero(): LocalHero {
+    return LocalHero(
+            name = this.name,
+            assetsPath = this.assetsPath,
+            mainAttr = this.mainAttr
+    )
+}
