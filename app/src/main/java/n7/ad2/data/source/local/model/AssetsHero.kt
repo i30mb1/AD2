38b8f15eebd2045b589a90a5d874b2fbddef4d7b
i30mb1 <fate.i30mb1@gmail.com>
2020-04-28 15:56:12 +0300

    return proper model from assets

--- app/src/main/java/n7/ad2/data/source/local/model/AssetsHero.kt
+++ app/src/main/java/n7/ad2/data/source/local/model/AssetsHero.kt
@@ -1,18 +1,21 @@
 package n7.ad2.data.source.local.model
 
-import androidx.room.ColumnInfo
-import androidx.room.PrimaryKey
-import com.squareup.moshi.JsonClass
-
 import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 
 
 @JsonClass(generateAdapter = true)
+data class AssetsHeroList(
+        @Json(name = "heroes")
+        val heroes: List<AssetsHero> = listOf()
+)
+
+@JsonClass(generateAdapter = true)
 data class AssetsHero(
-    @Json(name = "assetsPath")
-    val assetsPath: String = "",
-    @Json(name = "mainAttr")
-    val mainAttr: String = "",
-    @Json(name = "nameEng")
-    val name: String = ""
+        @Json(name = "nameEng")
+        val name: String = "",
+        @Json(name = "assetsPath")
+        val assetsPath: String = "",
+        @Json(name = "mainAttr")
+        val mainAttr: String = ""
 )
