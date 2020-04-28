79afc8a530af16193e7123ba28b160338d35812b
i30mb1 <fate.i30mb1@gmail.com>
2020-04-28 13:37:38 +0300

    create empty worker for filling up my db

---
+++ app/src/main/java/n7/ad2/workers/DatabaseWorker.kt
@@ -0,0 +1,25 @@
+package n7.ad2.workers
+
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import kotlinx.coroutines.coroutineScope
+import n7.ad2.data.source.local.db.AppDatabase
+import n7.ad2.ui.MyApplication
+import javax.inject.Inject
+
+class DatabaseWorker(
+        val context: Context,
+        workerParameters: WorkerParameters
+) : CoroutineWorker(context, workerParameters) {
+
+    @Inject
+    private lateinit var appDatabase: AppDatabase
+
+    override suspend fun doWork(): Result = coroutineScope {
+        (context as MyApplication).component.inject(this@DatabaseWorker)
+
+        Result.success()
+    }
+
+}
