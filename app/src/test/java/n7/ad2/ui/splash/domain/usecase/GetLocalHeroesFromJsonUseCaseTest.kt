549c9ec0a4d4886ba3f7e510b7f12f90a516f6d5
i30mb1 <fate.i30mb1@gmail.com>
2020-04-28 15:07:24 +0300

    write test for use case

--- app/src/test/java/n7/ad2/ui/splash/domain/usecase/GetLocalHeroesFromJsonUseCaseTest.kt
+++ app/src/test/java/n7/ad2/ui/splash/domain/usecase/GetLocalHeroesFromJsonUseCaseTest.kt
@@ -1,8 +1,10 @@
 package n7.ad2.ui.splash.domain.usecase
 
+import com.google.common.truth.Truth.assertThat
 import com.squareup.moshi.Moshi
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import n7.ad2.CoroutineTestRule
+import n7.ad2.data.source.local.model.LocalHero
 import n7.ad2.runBlockingTest
 import org.junit.Rule
 import org.junit.Test
@@ -25,4 +27,22 @@ class GetLocalHeroesFromJsonUseCaseTest {
         getLocalHeroesFromFileUseCase(jsonString)
     }
 
+    @Test
+    fun `return object with fields from json`() = coroutineTestRule.runBlockingTest {
+        val testObject = LocalHero("Abaddon", "heroes2\\Abaddon", "Strength")
+        val jsonString = """
+                {
+                  "heroes": [
+                    {
+                      "nameEng": ${testObject.name}",
+                      "assetsPath": ${testObject.assetsPath},
+                      "mainAttr": ${testObject.mainAttr}
+                    }
+                            ] 
+                }
+        """.trimIndent()
+
+        val list = getLocalHeroesFromFileUseCase(jsonString)[0]
+        assertThat(list).isEqualTo(testObject)
+    }
 }
