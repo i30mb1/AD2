f420c668509710e5c33a0cc851f63bcfe96eecc2
i30mb1 <fate.i30mb1@gmail.com>
2020-05-02 12:45:44 +0300

    fill VO with data

--- app/src/main/java/n7/ad2/ui/heroes/domain/adapter/HeroAdapter.kt
+++ app/src/main/java/n7/ad2/ui/heroes/domain/adapter/HeroAdapter.kt
@@ -2,9 +2,11 @@ package n7.ad2.ui.heroes.domain.adapter
 
 import n7.ad2.data.source.local.model.LocalHero
 import n7.ad2.ui.heroes.domain.vo.VOHero
+import java.io.File
 
 fun LocalHero.toVo() : VOHero {
     return VOHero().also {
-
+        it.name = name
+        it.image = "$assetsPath\\full.webp"
     }
 }
