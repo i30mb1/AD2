da1f394d9077df6d7dabbc4b68cde054418da4f6
i30mb1 <fate.i30mb1@gmail.com>
2020-05-02 12:11:31 +0300

    delete redundant code

--- app/src/main/java/n7/ad2/heroes/full/HeroFulViewModel.java
+++ app/src/main/java/n7/ad2/heroes/full/HeroFulViewModel.java
@@ -3,19 +3,10 @@ package n7.ad2.heroes.full;
 import android.Manifest;
 import android.app.Application;
 import android.app.DownloadManager;
-import androidx.lifecycle.AndroidViewModel;
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.Observer;
-import androidx.paging.LivePagedListBuilder;
-import androidx.paging.PagedList;
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
-import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
-import androidx.databinding.ObservableBoolean;
-import androidx.databinding.ObservableInt;
 import android.media.MediaPlayer;
 import android.media.RingtoneManager;
 import android.net.ConnectivityManager;
@@ -28,26 +19,23 @@ import android.os.Looper;
 import android.preference.PreferenceManager;
 import android.provider.MediaStore;
 import android.provider.Settings;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
-
+import androidx.databinding.ObservableBoolean;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
+import androidx.paging.LivePagedListBuilder;
+import androidx.paging.PagedList;
 import androidx.work.Constraints;
 import androidx.work.Data;
 import androidx.work.NetworkType;
 import androidx.work.OneTimeWorkRequest;
 import androidx.work.WorkInfo;
 import androidx.work.WorkManager;
-
-//import com.google.android.exoplayer2.ExoPlaybackException;
-//import com.google.android.exoplayer2.ExoPlayerFactory;
-//import com.google.android.exoplayer2.Player;
-//import com.google.android.exoplayer2.SimpleExoPlayer;
-//import com.google.android.exoplayer2.source.MediaSource;
-//import com.google.android.exoplayer2.source.ProgressiveMediaSource;
-//import com.google.android.exoplayer2.upstream.DataSource;
-//import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
-//import com.google.android.exoplayer2.util.Util;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -67,20 +55,26 @@ import n7.ad2.utils.Utils;
 
 import static n7.ad2.heroes.full.HeroFullActivity.HERO_CODE_NAME;
 
-public class HeroFulViewModel extends AndroidViewModel implements SharedPreferences.OnSharedPreferenceChangeListener {
+//import com.google.android.exoplayer2.ExoPlaybackException;
+//import com.google.android.exoplayer2.ExoPlayerFactory;
+//import com.google.android.exoplayer2.Player;
+//import com.google.android.exoplayer2.SimpleExoPlayer;
+//import com.google.android.exoplayer2.source.MediaSource;
+//import com.google.android.exoplayer2.source.ProgressiveMediaSource;
+//import com.google.android.exoplayer2.upstream.DataSource;
+//import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+//import com.google.android.exoplayer2.util.Util;
 
-    public static final String FREE_RESPONSE_FOR_DAY_KEY = "FREE_RESPONSE_FOR_DAY_KEY";
-    public static final int FREE_RESPONSE_FOR_DAY = 10;
+public class HeroFulViewModel extends AndroidViewModel {
+
     public static final int FILE_EXIST = -7;
     private final static Executor diskIO = Executors.newSingleThreadExecutor();
     public final SnackbarMessage grandSetting = new SnackbarMessage();
     public final SnackbarMessage grandPermission = new SnackbarMessage();
     public final SnackbarMessage showSnackBar = new SnackbarMessage();
-    public ObservableInt counterFreeResponses = new ObservableInt(FREE_RESPONSE_FOR_DAY);
     public MutableLiveData<JSONObject> jsonObjectHeroFull = new MutableLiveData<>();
     public MutableLiveData<JSONArray> jsonArrayHeroAbilities = new MutableLiveData<>();
     public ObservableBoolean isGuideLoading = new ObservableBoolean(false);
-    public ObservableBoolean canDownloadResponses = new ObservableBoolean(true);
     public ObservableBoolean userSubscription = new ObservableBoolean(false);
     public MutableLiveData<HeroModel> hero = new MutableLiveData<>();
     public LinkedList<String> responsesInMemory = new LinkedList<>();
@@ -105,30 +99,7 @@ public class HeroFulViewModel extends AndroidViewModel implements SharedPreferen
         loadAvailableResponsesInMemory();
         loadResponses(application.getString(R.string.language_resource));
         laodHeroByCodeName(heroCode);
-        loadFreeResponses();
         loadUserSubscription();
-    }
-
-    @Override
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        if (key.equals(FREE_RESPONSE_FOR_DAY_KEY)) {
-            loadFreeResponses();
-        }
-    }
-
-    private void loadFreeResponses() {
-        PreferenceManager.getDefaultSharedPreferences(application).registerOnSharedPreferenceChangeListener(this);
-        int counter = PreferenceManager.getDefaultSharedPreferences(application).getInt(FREE_RESPONSE_FOR_DAY_KEY, FREE_RESPONSE_FOR_DAY);
-        counterFreeResponses.set(counter);
-        if (counter > 0) {
-            canDownloadResponses.set(true);
-        } else {
-            canDownloadResponses.set(false);
-        }
-    }
-
-    public void decreaseFreeResponses() {
-        PreferenceManager.getDefaultSharedPreferences(application).edit().putInt(FREE_RESPONSE_FOR_DAY_KEY, counterFreeResponses.get() - 1).apply();
     }
 
     private void loadFreshGuideForHero(final HeroModel heroModel) {
@@ -136,7 +107,6 @@ public class HeroFulViewModel extends AndroidViewModel implements SharedPreferen
         int currentDay = PreferenceManager.getDefaultSharedPreferences(application).getInt(application.getString(R.string.setting_current_day), 0);
         int guideLastDay = heroModel.getGuideLastDay();
         if (currentDay != guideLastDay) {
-            PreferenceManager.getDefaultSharedPreferences(application).edit().putInt(FREE_RESPONSE_FOR_DAY_KEY, FREE_RESPONSE_FOR_DAY).apply();
             Constraints constraints = new Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED).build();
             Data data = new Data.Builder().putString(HERO_CODE_NAME, heroCode).build();
             final OneTimeWorkRequest worker = new OneTimeWorkRequest.Builder(GuideWorker.class)
@@ -164,8 +134,6 @@ public class HeroFulViewModel extends AndroidViewModel implements SharedPreferen
 
         }
     }
-
-
 
 
     private void laodHeroByCodeName(final String heroCode) {
@@ -375,7 +343,6 @@ public class HeroFulViewModel extends AndroidViewModel implements SharedPreferen
             if (file.exists()) {
                 showSnackBar.postValue(FILE_EXIST);
             } else {
-                if (!userSubscription.get()) decreaseFreeResponses();
                 DownloadManager manager = (DownloadManager) application.getSystemService(Context.DOWNLOAD_SERVICE);
                 if (manager != null) {
                     manager.enqueue(new DownloadManager.Request(Uri.parse(model.getHref()))
