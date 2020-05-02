ed7de98e3f0642b26659bb4c614f95686e26941c
i30mb1 <fate.i30mb1@gmail.com>
2020-05-02 13:42:03 +0300

    add Annotation @Inject for UseCase

--- app/src/main/java/n7/ad2/ui/heroes/domain/usecase/ConvertLocalHeroListToVoListUseCase.kt
+++ app/src/main/java/n7/ad2/ui/heroes/domain/usecase/ConvertLocalHeroListToVoListUseCase.kt
@@ -5,8 +5,9 @@ import kotlinx.coroutines.withContext
 import n7.ad2.data.source.local.model.LocalHero
 import n7.ad2.ui.heroes.domain.adapter.toVo
 import n7.ad2.ui.heroes.domain.vo.VOHero
+import javax.inject.Inject
 
-class ConvertLocalHeroListToVoListUseCase constructor(
+class ConvertLocalHeroListToVoListUseCase @Inject constructor(
         private val ioDispatcher: CoroutineDispatcher
 ) {
 
