c5442736c5e2dcedc85dacd3743cdc79aff1a642
i30mb1 <fate.i30mb1@gmail.com>
2020-05-16 15:49:09 +0300

    add color spans for async text

--- app/src/main/java/n7/ad2/utils/BindingAdapters.kt
+++ app/src/main/java/n7/ad2/utils/BindingAdapters.kt
@@ -1,11 +1,15 @@
 package n7.ad2.utils
 
+import android.graphics.Color
 import android.graphics.drawable.Drawable
+import android.text.style.ForegroundColorSpan
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.core.text.PrecomputedTextCompat
+import androidx.core.text.set
+import androidx.core.text.toSpannable
 import androidx.core.widget.TextViewCompat
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.ListAdapter
@@ -21,6 +25,7 @@ fun ImageView.loadImageUrl(url: String?, error: Drawable?, placeHolder: Drawable
     }
 }
 
+@Suppress("UNCHECKED_CAST")
 @BindingAdapter("data")
 fun <T> RecyclerView.loadData(list: List<T>?) {
     (this.adapter as ListAdapter<T, RecyclerView.ViewHolder>).submitList(list)
@@ -32,17 +37,26 @@ fun View.isVisible(isVisible: Boolean) {
 }
 
 @BindingAdapter("asyncText", "android:textSize", "withDash", requireAll = false)
-fun asyncText(textView: TextView, text: CharSequence, textSize: Int?, withDash: Boolean) {
+fun asyncText(textView: TextView, text: CharSequence, textSize: Int?, withDash: Boolean = false) {
     // first, set all measurement affecting properties of the text
     // (size, locale, typeface, direction, etc)
     if (textSize != null) {
         // interpret the text size as SP
         textView.textSize = textSize.toFloat()
     }
+
+    val spannable = text.toSpannable()
+    if (withDash) {
+//        val index = spannable.indexOf("-")
+//        while (index != -1) {
+        spannable[0, 10] = ForegroundColorSpan(Color.RED)
+//        }
+    }
+
     val params = TextViewCompat.getTextMetricsParams(textView)
     (textView as AppCompatTextView).setTextFuture(
             PrecomputedTextCompat.getTextFuture(
-                    text,
+                    spannable,
                     params,
                     null
             )
