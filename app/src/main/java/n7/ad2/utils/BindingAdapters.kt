45d3d8ff241f22d8b859d99a5f0e30fe8be7d5e2
i30mb1 <fate.i30mb1@gmail.com>
2020-05-16 15:18:10 +0300

    replace ordinary text setter on async setter

--- app/src/main/java/n7/ad2/utils/BindingAdapters.kt
+++ app/src/main/java/n7/ad2/utils/BindingAdapters.kt
@@ -3,11 +3,14 @@ package n7.ad2.utils
 import android.graphics.drawable.Drawable
 import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.text.PrecomputedTextCompat
+import androidx.core.widget.TextViewCompat
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import coil.api.load
-import n7.ad2.ui.heroInfo.domain.vo.VOSpell
 
 @BindingAdapter("loadImageUrl", "onError", "placeHolder", requireAll = false)
 fun ImageView.loadImageUrl(url: String?, error: Drawable?, placeHolder: Drawable?) {
@@ -25,5 +28,23 @@ fun <T> RecyclerView.loadData(list: List<T>?) {
 
 @BindingAdapter("isVisible")
 fun View.isVisible(isVisible: Boolean) {
-    this.visibility = if(isVisible) View.VISIBLE else View.GONE
+    this.visibility = if (isVisible) View.VISIBLE else View.GONE
+}
+
+@BindingAdapter("asyncText", "android:textSize", "withDash", requireAll = false)
+fun asyncText(textView: TextView, text: CharSequence, textSize: Int?, withDash: Boolean) {
+    // first, set all measurement affecting properties of the text
+    // (size, locale, typeface, direction, etc)
+    if (textSize != null) {
+        // interpret the text size as SP
+        textView.textSize = textSize.toFloat()
+    }
+    val params = TextViewCompat.getTextMetricsParams(textView)
+    (textView as AppCompatTextView).setTextFuture(
+            PrecomputedTextCompat.getTextFuture(
+                    text,
+                    params,
+                    null
+            )
+    )
 }
