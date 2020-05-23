487b6850d45856541efc86779a33c8b3de377b5d
i30mb1 <fate.i30mb1@gmail.com>
2020-05-23 19:18:37 +0300

    add extension that i will use in near future... looks shitty...

--- app/src/main/java/n7/ad2/ui/heroInfo/domain/usecase/GetVOHeroDescriptionUseCase.kt
+++ app/src/main/java/n7/ad2/ui/heroInfo/domain/usecase/GetVOHeroDescriptionUseCase.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import android.content.res.Resources
 import android.text.Spannable
 import android.text.SpannableString
+import android.text.SpannableStringBuilder
 import android.text.style.ClickableSpan
 import android.text.style.DynamicDrawableSpan
 import android.text.style.ImageSpan
@@ -27,6 +28,26 @@ class GetVOHeroDescriptionUseCase @Inject constructor(
     companion object {
         const val SEPARATOR = "- "
         const val TAG_TALENT = "TagTalent"
+        const val COLON = ": "
+    }
+
+    private fun List<String>.toStringListWithDashAfterColon(): String {
+        val builder = SpannableStringBuilder()
+        forEachIndexed { index, text ->
+            builder.append(text)
+            if (index != lastIndex) builder.append(System.lineSeparator())
+        }
+        var startIndex = 0
+        var indexOf = builder.indexOf(COLON, startIndex)
+        if (indexOf == -1) return builder.toString()
+
+        do {
+            builder.insert(indexOf + 1,"\n")
+            startIndex++
+            indexOf = builder.indexOf(COLON, startIndex)
+        } while (indexOf!= -1)
+
+        return builder.toString()
     }
 
     private fun List<String>.toStringListWithDash(): String {
