2a375eec29112010ecbb8d175dcb5ecaa7f229c1
i30mb1 <fate.i30mb1@gmail.com>
2020-05-27 16:50:21 +0300

    extract group of useCases in Interactor that perform execution https://youtu.be/AlxMGxs2QnM?t=1113

---
+++ app/src/main/java/n7/ad2/ui/splash/domain/interactor/PopulateHeroesDatabaseInteractor.kt
@@ -0,0 +1,24 @@
+package n7.ad2.ui.splash.domain.interactor
+
+import n7.ad2.ui.splash.domain.usecase.ConvertAssetsHeroListToLocalHeroListUseCase
+import n7.ad2.ui.splash.domain.usecase.ConvertJsonHeroesToAssetsHeroesUseCase
+import n7.ad2.ui.splash.domain.usecase.GetJsonHeroesFromAssetsUseCase
+import n7.ad2.ui.splash.domain.usecase.SaveLocalHeroesInDatabaseUseCase
+import javax.inject.Inject
+
+class PopulateHeroesDatabaseInteractor @Inject constructor(
+      private val saveLocalHeroesInDatabaseUseCase: SaveLocalHeroesInDatabaseUseCase,
+      private val getJsonHeroesFromAssetsUseCase: GetJsonHeroesFromAssetsUseCase,
+      private val convertJsonHeroesToAssetsHeroesUseCase: ConvertJsonHeroesToAssetsHeroesUseCase,
+      private val convertAssetsHeroListToLocalHeroListUseCase: ConvertAssetsHeroListToLocalHeroListUseCase
+) {
+
+    suspend operator fun invoke() {
+        val json: String = getJsonHeroesFromAssetsUseCase()
+        val assetsHeroesList = convertJsonHeroesToAssetsHeroesUseCase(json)
+        val localHeroesList = convertAssetsHeroListToLocalHeroListUseCase(assetsHeroesList)
+
+        saveLocalHeroesInDatabaseUseCase(localHeroesList)
+    }
+
+}
--- app/src/main/java/n7/ad2/workers/DatabaseWorker.kt
+++ app/src/main/java/n7/ad2/workers/DatabaseWorker.kt
@@ -5,10 +5,7 @@ import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import kotlinx.coroutines.coroutineScope
 import n7.ad2.ui.MyApplication
-import n7.ad2.ui.splash.domain.usecase.ConvertAssetsHeroListToLocalHeroListUseCase
-import n7.ad2.ui.splash.domain.usecase.ConvertJsonHeroesToAssetsHeroesUseCase
-import n7.ad2.ui.splash.domain.usecase.GetJsonHeroesFromAssetsUseCase
-import n7.ad2.ui.splash.domain.usecase.SaveLocalHeroesInDatabaseUseCase
+import n7.ad2.ui.splash.domain.interactor.PopulateHeroesDatabaseInteractor
 import javax.inject.Inject
 
 class DatabaseWorker(
@@ -17,25 +14,12 @@ class DatabaseWorker(
 ) : CoroutineWorker(context, workerParameters) {
 
     @Inject
-    lateinit var saveLocalHeroesInDatabaseUseCase: SaveLocalHeroesInDatabaseUseCase
-
-    @Inject
-    lateinit var getJsonHeroesFromAssetsUseCase: GetJsonHeroesFromAssetsUseCase
-
-    @Inject
-    lateinit var convertJsonHeroesToAssetsHeroesUseCase: ConvertJsonHeroesToAssetsHeroesUseCase
-
-    @Inject
-    lateinit var convertAssetsHeroListToLocalHeroListUseCase: ConvertAssetsHeroListToLocalHeroListUseCase
+    lateinit var populateHeroesDatabaseInteractor: PopulateHeroesDatabaseInteractor
 
     override suspend fun doWork(): Result = coroutineScope {
         (context as MyApplication).component.inject(this@DatabaseWorker)
 
-        val json: String = getJsonHeroesFromAssetsUseCase()
-        val assetsHeroesList = convertJsonHeroesToAssetsHeroesUseCase(json)
-        val localHeroesList = convertAssetsHeroListToLocalHeroListUseCase(assetsHeroesList)
-
-        saveLocalHeroesInDatabaseUseCase(localHeroesList)
+        populateHeroesDatabaseInteractor.invoke()
 
         Result.success()
     }
