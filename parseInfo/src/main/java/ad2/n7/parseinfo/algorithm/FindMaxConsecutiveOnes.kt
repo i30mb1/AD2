386f180ecb82634f0a01bc94efeb51840b2baea3
i30mb1 <fate.i30mb1@gmail.com>
2020-06-13 09:31:00 +0300

    add another algorithm for sortedSquare

--- parseInfo/src/main/java/ad2/n7/parseinfo/algorithm/FindMaxConsecutiveOnes.kt
+++ parseInfo/src/main/java/ad2/n7/parseinfo/algorithm/FindMaxConsecutiveOnes.kt
@@ -1,5 +1,7 @@
 package ad2.n7.parseinfo.algorithm
 
+import kotlin.math.abs
+
 fun main() {
     println(sortedSquares(intArrayOf(-3, 1, 0, 3, 15)))
 }
@@ -29,5 +31,29 @@ fun findInnerNumberOfNumber(number: Int): Int {
 
 fun sortedSquares(array: IntArray): IntArray {
    return array.map { it * it }.sorted().toIntArray()
+}
+
+fun sortedSquares2(array: IntArray): IntArray {
+    var leftMarker = 0
+    var rightMarker = array.size - 1
+    var currentMarker = array.size -1
+
+    val result = IntArray(array.size)
+
+    while (leftMarker <= rightMarker){
+        val left = abs(array[leftMarker])
+        val right = abs(array[rightMarker])
+
+        result[currentMarker] = if (right > left) {
+            rightMarker--
+            right * right
+        } else {
+            leftMarker++
+            left * left
+        }
+
+        currentMarker--
+    }
+    return result
 }
 
