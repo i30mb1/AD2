05f61492bd919c9ad117e843ea50c2832756753f
i30mb1 <fate.i30mb1@gmail.com>
2020-06-18 20:52:22 +0300

    add model for HeroResponses

---
+++ app/src/main/java/n7/ad2/ui/heroInfo/domain/model/HeroResponses.kt
@@ -0,0 +1,22 @@
+package n7.ad2.ui.heroInfo.domain.model
+import com.squareup.moshi.JsonClass
+
+import com.squareup.moshi.Json
+
+class HeroResponses : ArrayList<HeroResponsesItem>()
+
+@JsonClass(generateAdapter = true)
+data class HeroResponsesItem(
+    @Json(name = "category")
+    val category: String = "",
+    @Json(name = "responses")
+    val responses: List<Response> = listOf()
+)
+
+@JsonClass(generateAdapter = true)
+data class Response(
+    @Json(name = "audioUrl")
+    val audioUrl: String = "",
+    @Json(name = "title")
+    val title: String = ""
+)
