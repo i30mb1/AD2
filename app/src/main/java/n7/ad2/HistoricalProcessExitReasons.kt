03f4d0c49bf8bc2f41c22663931a34b0ed9d44fe
i30mb1 <fate.i30mb1@gmail.com>
2020-06-18 11:57:51 +0300

    after update on android11 i will come in handy https://youtu.be/fnkFOhA7FC4?t=1005

---
+++ app/src/main/java/n7/ad2/HistoricalProcessExitReasons.kt
@@ -0,0 +1,16 @@
+package n7.ad2
+
+import android.app.ActivityManager
+
+class HistoricalProcessExitReasons {
+
+    fun get(activityManager: ActivityManager) {
+//        val reasonsList = activityManager.getHistoricalProcessExitReasons()
+//        for(info in resonsList) {
+//
+//        }
+
+    }
+
+
+}
