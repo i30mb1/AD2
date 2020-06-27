6449570b8150977ad0f21b28ad1c2e8446b94627
i30mb1 <fate.i30mb1@gmail.com>
2020-06-27 14:43:16 +0300

    add lifecycle to InfoPopupWindow.kt

--- app/src/main/java/n7/ad2/ui/heroInfo/InfoPopupWindow.kt
+++ app/src/main/java/n7/ad2/ui/heroInfo/InfoPopupWindow.kt
@@ -1,5 +1,6 @@
 package n7.ad2.ui.heroInfo
 
+import android.content.Context
 import android.graphics.Point
 import android.view.Gravity
 import android.view.LayoutInflater
@@ -10,42 +11,48 @@ import android.widget.PopupWindow
 import androidx.core.graphics.component1
 import androidx.core.graphics.component2
 import androidx.core.view.doOnPreDraw
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
 import n7.ad2.databinding.PopupSpellInfoBinding
 
-class InfoPopupWindow(private val anchor: View, private val text: String) {
+class InfoPopupWindow(
+        context: Context,
+        private val lifecycle: Lifecycle
+) : LifecycleObserver {
 
-    private val binding = PopupSpellInfoBinding.inflate(LayoutInflater.from(anchor.context))
+    private val binding = PopupSpellInfoBinding.inflate(LayoutInflater.from(context))
     private val popup: PopupWindow
 
     init {
         popup = PopupWindow(binding.root, LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT).apply {
             isOutsideTouchable = true
-            showAsDropDown(anchor, 0, 0, Gravity.BOTTOM)
         }
 
-        binding.root.doOnPreDraw {
-            updatePointerLocation()
-        }
+        binding.body.setOnClickListener { popup.dismiss() }
+    }
+
+    fun show(anchor: View, text: String) {
         binding.body.text = text
-        binding.body.setOnClickListener {
-            popup.dismiss()
-        }
-
+        popup.showAsDropDown(anchor, 0, 0, Gravity.BOTTOM)
+        binding.root.doOnPreDraw { updatePointerLocation(anchor) }
     }
 
-    fun dismiss() {
-        popup.dismiss()
-    }
+    @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+    private fun dismiss() = popup.dismiss()
 
-    private fun updatePointerLocation() {
-        val (x, y) = getLocationOnScreen()
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    private fun onDestroy() = lifecycle.removeObserver(this)
+
+    private fun updatePointerLocation(anchor: View) {
+        val (x, _) = getLocationOnScreen(anchor)
 
         val params = binding.pointer.layoutParams as ViewGroup.MarginLayoutParams
         params.leftMargin = x
         binding.pointer.layoutParams = params
     }
 
-    private fun getLocationOnScreen(): Point {
+    private fun getLocationOnScreen(anchor: View): Point {
         val location = IntArray(2)
         anchor.getLocationOnScreen(location)
         return Point(location[0] + anchor.pivotX.toInt() - binding.pointer.width / 2, location[1])
