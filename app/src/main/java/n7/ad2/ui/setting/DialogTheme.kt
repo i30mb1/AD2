6eff03969a75069627f21e50a727e465ef51a7c8
i30mb1 <fate.i30mb1@gmail.com>
2020-07-12 19:55:56 +0300

    rename

--- app/src/main/java/n7/ad2/ui/setting/DialogTheme.kt
+++ app/src/main/java/n7/ad2/ui/setting/DialogTheme.kt
@@ -7,7 +7,7 @@ import android.util.AttributeSet
 import androidx.fragment.app.DialogFragment
 import androidx.preference.EditTextPreference
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import n7.ad2.databinding.DialogSettingFragmentThemeBinding
+import n7.ad2.databinding.DialogThemeBinding
 import n7.ad2.ui.setting.SettingsFragment.Companion.THEME_DARK
 import n7.ad2.ui.setting.SettingsFragment.Companion.THEME_GRAY
 import n7.ad2.ui.setting.SettingsFragment.Companion.THEME_WHITE
@@ -17,7 +17,7 @@ class DialogTheme : DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val settingsFragment = parentFragment as SettingsFragment
 
-        val dialogView = DialogSettingFragmentThemeBinding.inflate(layoutInflater).apply {
+        val dialogView = DialogThemeBinding.inflate(layoutInflater).apply {
             bDark.setOnClickListener { settingsFragment.applyTheme(THEME_DARK) }
             bGray.setOnClickListener { settingsFragment.applyTheme(THEME_GRAY) }
             bWhite.setOnClickListener { settingsFragment.applyTheme(THEME_WHITE) }
