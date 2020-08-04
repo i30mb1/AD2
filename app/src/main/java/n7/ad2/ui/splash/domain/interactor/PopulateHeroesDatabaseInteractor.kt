6e72684be8a2616aa81fad6e1230fed8a459acea
i30mb1 <fate.i30mb1@gmail.com>
2020-08-04 20:30:17 +0300

    reorder operation in constructor

--- app/src/main/java/n7/ad2/ui/splash/domain/interactor/PopulateHeroesDatabaseInteractor.kt
+++ app/src/main/java/n7/ad2/ui/splash/domain/interactor/PopulateHeroesDatabaseInteractor.kt
@@ -7,10 +7,10 @@ import n7.ad2.ui.splash.domain.usecase.SaveLocalHeroesInDatabaseUseCase
 import javax.inject.Inject
 
 class PopulateHeroesDatabaseInteractor @Inject constructor(
-      private val saveLocalHeroesInDatabaseUseCase: SaveLocalHeroesInDatabaseUseCase,
-      private val getJsonHeroesFromAssetsUseCase: GetJsonHeroesFromAssetsUseCase,
-      private val convertJsonHeroesToAssetsHeroesUseCase: ConvertJsonHeroesToAssetsHeroesUseCase,
-      private val convertAssetsHeroListToLocalHeroListUseCase: ConvertAssetsHeroListToLocalHeroListUseCase
+    private val getJsonHeroesFromAssetsUseCase: GetJsonHeroesFromAssetsUseCase,
+    private val convertJsonHeroesToAssetsHeroesUseCase: ConvertJsonHeroesToAssetsHeroesUseCase,
+    private val convertAssetsHeroListToLocalHeroListUseCase: ConvertAssetsHeroListToLocalHeroListUseCase,
+    private val saveLocalHeroesInDatabaseUseCase: SaveLocalHeroesInDatabaseUseCase
 ) {
 
     suspend operator fun invoke() {
