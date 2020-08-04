717ef64c1183b99d2041defe9273dfc2b98e8dc5
i30mb1 <fate.i30mb1@gmail.com>
2020-08-04 20:15:32 +0300

    add LocalItem.kt for room

---
+++ app/src/main/java/n7/ad2/data/source/local/model/LocalItem.kt
@@ -0,0 +1,18 @@
+package n7.ad2.data.source.local.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "LocalItems")
+data class LocalItem(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "rowid")
+    val id: Int = 0,
+    @ColumnInfo(name = "name")
+    val name: String,
+    @ColumnInfo(name = "assetsPath")
+    val assetsPath: String,
+    @ColumnInfo(name = "type")
+    val mainAttr: String
+)
