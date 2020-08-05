502a5dd75fb4d2d353187853e96223e9ba8fe229
i30mb1 <fate.i30mb1@gmail.com>
2020-08-05 19:41:36 +0300

    update material version

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -26,7 +26,7 @@ object Lib {
     const val retrofit         = "com.squareup.retrofit2:retrofit:2.7.1"
     const val gsonConverter    = "com.squareup.retrofit2:converter-gson:2.6.2"
     const val paging           = "androidx.paging:paging-runtime-ktx:2.1.2"
-    const val material         = "com.google.android.material:material:1.2.0-alpha02"
+    const val material         = "com.google.android.material:material:1.2.0"
     const val constraintLayout = "androidx.constraintlayout:constraintlayout:1.1.3"
     const val coil             = "io.coil-kt:coil:0.9.5"
     const val picasso          = "com.squareup.picasso:picasso:2.71828"
