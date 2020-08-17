4fcae7252b62fd98b74171d50955792ad31b71f9
i30mb1 <fate.i30mb1@gmail.com>
2020-08-17 19:58:14 +0300

    add PopulateItemsDatabaseInteractor.kt

--- app/src/main/java/n7/ad2/ui/splash/domain/interactor/PopulateItemsDatabaseInteractor.kt
+++ app/src/main/java/n7/ad2/ui/splash/domain/interactor/PopulateItemsDatabaseInteractor.kt
@@ -1,17 +1,24 @@
 package n7.ad2.ui.splash.domain.interactor
 
+import n7.ad2.ui.splash.domain.usecase.ConvertAssetsItemListToLocalItemListUseCase
 import n7.ad2.ui.splash.domain.usecase.ConvertJsonItemsToAssetsItemUseCase
 import n7.ad2.ui.splash.domain.usecase.GetJsonItemsFromAssetsUseCase
+import n7.ad2.ui.splash.domain.usecase.SaveLocalItemsInDatabaseUseCase
 import javax.inject.Inject
 
 class PopulateItemsDatabaseInteractor @Inject constructor(
     private val getJsonItemsFromAssetsUseCase: GetJsonItemsFromAssetsUseCase,
-    private val convertJsonItemsToAssetsItemUseCase: ConvertJsonItemsToAssetsItemUseCase
+    private val convertJsonItemsToAssetsItemUseCase: ConvertJsonItemsToAssetsItemUseCase,
+    private val convertAssetsItemListToLocalItemListUseCase: ConvertAssetsItemListToLocalItemListUseCase,
+    private val saveLocalItemsInDatabaseUseCase: SaveLocalItemsInDatabaseUseCase
 ) {
 
     suspend operator fun invoke() {
         val json = getJsonItemsFromAssetsUseCase()
-        val assetsItemList = convertJsonItemsToAssetsItemUseCase(json)
+        val assetsItemsList = convertJsonItemsToAssetsItemUseCase(json)
+
+        val localItemsList = convertAssetsItemListToLocalItemListUseCase(assetsItemsList)
+        saveLocalItemsInDatabaseUseCase(localItemsList)
     }
 
 }
