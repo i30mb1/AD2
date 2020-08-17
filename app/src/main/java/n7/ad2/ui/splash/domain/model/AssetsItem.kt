8a6b337fd37403be55354f699e31f9ea60607c11
i30mb1 <fate.i30mb1@gmail.com>
2020-08-17 19:37:16 +0300

    add assetsItem model

--- app/src/main/java/n7/ad2/ui/splash/domain/model/AssetsItem.kt
+++ app/src/main/java/n7/ad2/ui/splash/domain/model/AssetsItem.kt
@@ -3,43 +3,18 @@ import com.squareup.moshi.JsonClass
 
 import com.squareup.moshi.Json
 
-
 @JsonClass(generateAdapter = true)
-data class AssetsItem(
-    @Json(name = "abilities")
-    val abilities: List<Ability> = listOf(),
-    @Json(name = "additionalInformation")
-    val additionalInformation: List<String> = listOf(),
-    @Json(name = "description")
-    val description: String = "",
-    @Json(name = "lore")
-    val lore: List<Any> = listOf(),
-    @Json(name = "name")
-    val name: String = "",
-    @Json(name = "tips")
-    val tips: List<String> = listOf()
+data class AssetsItemList(
+    @Json(name = "items")
+    val items: List<AssetsItem> = listOf()
 )
 
 @JsonClass(generateAdapter = true)
-data class Ability(
-    @Json(name = "abilityName")
-    val abilityName: String = "",
-    @Json(name = "audioUrl")
-    val audioUrl: String? = null,
-    @Json(name = "cooldown")
-    val cooldown: Any? = null,
-    @Json(name = "description")
-    val description: String = "",
-    @Json(name = "effects")
-    val effects: List<String> = listOf(),
-    @Json(name = "itemBehaviour")
-    val itemBehaviour: List<Any> = listOf(),
-    @Json(name = "mana")
-    val mana: String? = null,
-    @Json(name = "notes")
-    val notes: List<String> = listOf(),
-    @Json(name = "params")
-    val params: List<String> = listOf(),
-    @Json(name = "story")
-    val story: Any? = null
+data class AssetsItem(
+    @Json(name = "assetsPath")
+    val assetsPath: String = "",
+    @Json(name = "nameEng")
+    val nameEng: String = "",
+    @Json(name = "section")
+    val section: String = ""
 )
