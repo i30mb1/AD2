e87fdf72a4c411af47d4f7bb185510505d93e0a0
i30mb1 <fate.i30mb1@gmail.com>
2020-08-17 19:55:31 +0300

    add items table for db

--- app/src/main/java/n7/ad2/data/source/local/db/AppDatabase.kt
+++ app/src/main/java/n7/ad2/data/source/local/db/AppDatabase.kt
@@ -3,10 +3,16 @@ package n7.ad2.data.source.local.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import n7.ad2.data.source.local.model.LocalHero
+import n7.ad2.data.source.local.model.LocalItem
 
 private const val DB_VERSION = 1
 
-@Database(entities = [LocalHero::class], version = DB_VERSION, exportSchema = false)
+@Database(
+    entities = [
+        LocalHero::class,
+        LocalItem::class
+    ], version = DB_VERSION, exportSchema = false
+)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract val heroesDao: HeroesDao
