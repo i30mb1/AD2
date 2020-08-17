5173a6a7d3cd9c7063ccd0f9132f57a629b4751a
i30mb1 <fate.i30mb1@gmail.com>
2020-08-17 19:46:35 +0300

    add converter UseCase from AssetsItem to LocalItem

---
+++ app/src/main/java/n7/ad2/ui/splash/domain/usecase/ConvertAssetsItemListToLocalItemListUseCase.kt
@@ -0,0 +1,16 @@
+package n7.ad2.ui.splash.domain.usecase
+
+import n7.ad2.data.source.local.model.LocalItem
+import n7.ad2.ui.splash.domain.adapter.toLocalItem
+import n7.ad2.ui.splash.domain.model.AssetsItem
+import javax.inject.Inject
+
+class ConvertAssetsItemListToLocalItemListUseCase @Inject constructor() {
+
+    operator fun invoke(assetsItemList: List<AssetsItem>): List<LocalItem> {
+        return assetsItemList.map {
+            it.toLocalItem()
+        }
+    }
+
+}
