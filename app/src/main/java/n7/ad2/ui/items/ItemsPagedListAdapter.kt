a1bc180e8d60437a6482762c93e0fe6bdf590028
i30mb1 <fate.i30mb1@gmail.com>
2020-08-20 18:37:06 +0300

    refactoring listAdapter

---
+++ app/src/main/java/n7/ad2/ui/items/ItemsPagedListAdapter.kt
@@ -0,0 +1,66 @@
+package n7.ad2.ui.items
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.paging.PagedListAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import coil.api.clear
+import n7.ad2.base.VOModelListener
+import n7.ad2.databinding.ItemItemBinding
+import n7.ad2.ui.items.domain.vo.VOItem
+
+class ItemsPagedListAdapter constructor(fragment: ItemsFragment) : PagedListAdapter<VOItem, ItemsPagedListAdapter.ViewHolder>(DiffCallback()) {
+
+    private val listener = object : VOModelListener<VOItem> {
+        override fun onClickListener(model: VOItem) {
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder = ViewHolder.from(parent, listener)
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = getItem(position)
+        if (item != null) holder.bindTo(item) else holder.clear()
+    }
+
+    override fun onViewRecycled(holder: ViewHolder) {
+        super.onViewRecycled(holder)
+        holder.clear()
+    }
+
+    class ViewHolder(
+        private val binding: ItemItemBinding,
+        private val listener: VOModelListener<VOItem>
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bindTo(item: VOItem) = binding.let {
+            it.model = item
+            it.listener = listener
+            it.executePendingBindings()
+        }
+
+        fun clear() = binding.apply {
+            iv.clear()
+            tv.text = ""
+        }
+
+        companion object {
+            fun from(parent: ViewGroup, listener: VOModelListener<VOItem>): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = ItemItemBinding.inflate(layoutInflater, parent, false)
+                return ViewHolder(binding, listener)
+            }
+        }
+
+    }
+
+    private class DiffCallback : DiffUtil.ItemCallback<VOItem>() {
+        override fun areItemsTheSame(oldItem: VOItem, newItem: VOItem): Boolean = oldItem.name == newItem.name
+
+        override fun areContentsTheSame(oldItem: VOItem, newItem: VOItem): Boolean = true
+
+    }
+
+}
