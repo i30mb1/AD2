53ee49688903726f3b88135cfa7c508eb0d75a01
i30mb1 <fate.i30mb1@gmail.com>
2020-08-20 18:38:42 +0300

    add converter from LocalItem to VoItem

---
+++ app/src/main/java/n7/ad2/ui/items/domain/adapter/ItemAdapter.kt
@@ -0,0 +1,11 @@
+package n7.ad2.ui.items.domain.adapter
+
+import n7.ad2.data.source.local.model.LocalItem
+import n7.ad2.ui.items.domain.vo.VOItem
+
+fun LocalItem.toVO(): VOItem {
+    return VOItem().also {
+        it.name = name
+        it.image = "-"
+    }
+}
