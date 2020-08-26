4ee6038f94f5f23e3005379dc745773dd9eebfc1
i30mb1 <fate.i30mb1@gmail.com>
2020-08-26 19:55:22 +0300

    add notification to Worker, so it can be run instantly

--- app/src/main/java/n7/ad2/ui/heroGuide/HeroGuideWorker.kt
+++ app/src/main/java/n7/ad2/ui/heroGuide/HeroGuideWorker.kt
@@ -3,12 +3,16 @@
 package n7.ad2.ui.heroGuide
 
 import android.content.Context
+import androidx.core.app.NotificationCompat
 import androidx.work.CoroutineWorker
+import androidx.work.ForegroundInfo
 import androidx.work.WorkerParameters
-import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
 import kotlinx.coroutines.coroutineScope
+import n7.ad2.R
+import n7.ad2.createNotificationChannel
 import n7.ad2.data.source.local.model.LocalGuide
+import n7.ad2.isChannelNotCreated
 import n7.ad2.ui.MyApplication
 import n7.ad2.ui.heroGuide.domain.model.LocalGuideJsonModel
 import n7.ad2.ui.heroGuide.domain.usecase.SaveLocalGuideUseCase
@@ -26,6 +30,11 @@ class HeroGuideWorker(
         const val HERO_NAME = "HERO_NAME"
     }
 
+    private val notificationId = 1
+    private val channelId = "guide_worker"
+    private val channelName = applicationContext.getString(R.string.channel_guide)
+    private val notificationTitle = applicationContext.getString(R.string.notification_title_guide)
+
     @Inject
     lateinit var saveLocalGuideUseCase: SaveLocalGuideUseCase
 
@@ -33,6 +42,15 @@ class HeroGuideWorker(
     lateinit var moshi: Moshi
 
     override suspend fun doWork(): Result = coroutineScope {
+        if (applicationContext.isChannelNotCreated(channelId)) applicationContext.createNotificationChannel(channelId, channelName)
+
+        val notification = NotificationCompat.Builder(applicationContext, channelId)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle(notificationTitle)
+            .build()
+
+        setForeground(ForegroundInfo(notificationId, notification))
+
         (context as MyApplication).component.inject(this@HeroGuideWorker)
 
         try {
