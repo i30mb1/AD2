13703c93bd3b80e91caa45fc267bbdf6c78a4b24
i30mb1 <fate.i30mb1@gmail.com>
2020-08-26 19:50:51 +0300

    refactoring extensions for creating notification channel

--- app/src/main/java/n7/ad2/context.kt
+++ app/src/main/java/n7/ad2/context.kt
@@ -5,23 +5,22 @@ import android.app.NotificationManager
 import android.content.Context
 import android.os.Build
 
-fun Context.isChannelEnabled(channelId: String): Boolean {
+fun Context.isChannelNotCreated(channelId: String): Boolean {
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         val notificationChannel: NotificationChannel? = notificationManager.getNotificationChannel(channelId)
-        return notificationChannel?.importance != NotificationManager.IMPORTANCE_NONE
+        return notificationChannel?.importance == NotificationManager.IMPORTANCE_NONE
     }
-    return true
+    return false
 }
 
-const val CHANNEL_ID = "01_tournament"
-
-fun Context.createNotificationChannel() {
+fun Context.createNotificationChannel(channelId: String, channelName: String) {
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
         val channel = NotificationChannel(
-                CHANNEL_ID,
-                getString(R.string.channel_tournament_name),
-                NotificationManager.IMPORTANCE_HIGH)
+            channelId,
+            channelName,
+            NotificationManager.IMPORTANCE_HIGH
+        )
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         notificationManager.createNotificationChannel(channel)
     }
