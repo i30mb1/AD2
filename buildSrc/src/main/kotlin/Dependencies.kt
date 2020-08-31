088554035f5a1ba5e0530d2f62d62709a6929226
i30mb1 <fate.i30mb1@gmail.com>
2020-08-31 19:02:29 +0300

    update constraint layout version

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -28,7 +28,7 @@ object Lib {
     const val paging           = "androidx.paging:paging-runtime-ktx:2.1.2"
     const val paging3          = "androidx.paging:paging-runtime:3.0.0-alpha05"
     const val material         = "com.google.android.material:material:1.2.0"
-    const val constraintLayout = "androidx.constraintlayout:constraintlayout:1.1.3"
+    const val constraintLayout = "androidx.constraintlayout:constraintlayout:2.0.0"
     const val coil             = "io.coil-kt:coil:0.9.5"
     const val picasso          = "com.squareup.picasso:picasso:2.71828"
     const val picassoTrans     = "jp.wasabeef:picasso-transformations:2.2.1"
