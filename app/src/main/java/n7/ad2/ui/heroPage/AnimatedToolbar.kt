caa71639c1fc682ccfc09ab1a600e2ea8d0d674c
i30mb1 <fate.i30mb1@gmail.com>
2020-09-09 18:57:57 +0300

    create my custom AnimatedToolbar.kt

---
+++ app/src/main/java/n7/ad2/ui/heroPage/AnimatedToolbar.kt
@@ -0,0 +1,29 @@
+package n7.ad2.ui.heroPage
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.Gravity
+import android.widget.ImageView
+import androidx.appcompat.widget.Toolbar
+import coil.api.load
+import n7.ad2.data.source.local.Repository
+import n7.ad2.data.source.local.model.LocalHero
+import n7.ad2.utils.extension.toPx
+
+class AnimatedToolbar(context: Context, attr: AttributeSet) : Toolbar(context, attr) {
+
+    private var imageHero: ImageView = ImageView(context).apply {
+        val params = LayoutParams(30.toPx.toInt(), 30.toPx.toInt())
+        params.gravity = Gravity.CENTER
+        layoutParams = params
+        addView(this)
+    }
+
+    init {
+    }
+
+    fun loadHero(hero: LocalHero) {
+        imageHero.load("file:///android_asset/${hero.assetsPath}/${Repository.ASSETS_FILE_MINIMAP}")
+    }
+
+}
