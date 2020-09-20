48303ba1706d49fbd38b78fc9fbd26e93ad67649
i30mb1 <fate.i30mb1@gmail.com>
2020-09-20 16:43:42 +0300

    fix error

--- app/src/main/java/n7/ad2/ui/splash/domain/usecase/SaveLocalItemsInDatabaseUseCase.kt
+++ app/src/main/java/n7/ad2/ui/splash/domain/usecase/SaveLocalItemsInDatabaseUseCase.kt
@@ -2,13 +2,14 @@ package n7.ad2.ui.splash.domain.usecase
 
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
+import n7.ad2.data.source.local.ItemRepository
 import n7.ad2.data.source.local.Repository
 import n7.ad2.data.source.local.model.LocalHero
 import n7.ad2.data.source.local.model.LocalItem
 import javax.inject.Inject
 
 class SaveLocalItemsInDatabaseUseCase @Inject constructor(
-    private val repository: Repository,
+    private val repository: ItemRepository,
     private val ioDispatcher: CoroutineDispatcher
 ) {
 
