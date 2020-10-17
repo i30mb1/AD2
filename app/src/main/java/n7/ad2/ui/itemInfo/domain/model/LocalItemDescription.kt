b3a5a969644b40f3dc33475626d96dff1e0e4b01
i30mb1 <fate.i30mb1@gmail.com>
2020-10-17 14:27:34 +0300

    add LocalItemDescription.kt model

---
+++ app/src/main/java/n7/ad2/ui/itemInfo/domain/model/LocalItemDescription.kt
@@ -0,0 +1,44 @@
+package n7.ad2.ui.itemInfo.domain.model
+import com.squareup.moshi.JsonClass
+
+import com.squareup.moshi.Json
+
+@JsonClass(generateAdapter = true)
+data class LocalItemDescription(
+    @Json(name = "abilities")
+    val abilities: List<Ability> = listOf(),
+    @Json(name = "additionalInformation")
+    val additionalInformation: List<String> = listOf(),
+    @Json(name = "description")
+    val description: String = "",
+    @Json(name = "lore")
+    val lore: List<Any> = listOf(),
+    @Json(name = "name")
+    val name: String = "",
+    @Json(name = "tips")
+    val tips: List<String> = listOf()
+)
+
+@JsonClass(generateAdapter = true)
+data class Ability(
+    @Json(name = "abilityName")
+    val abilityName: String = "",
+    @Json(name = "audioUrl")
+    val audioUrl: String? = null,
+    @Json(name = "cooldown")
+    val cooldown: Any? = null,
+    @Json(name = "description")
+    val description: String = "",
+    @Json(name = "effects")
+    val effects: List<String> = listOf(),
+    @Json(name = "itemBehaviour")
+    val itemBehaviour: List<Any> = listOf(),
+    @Json(name = "mana")
+    val mana: String? = null,
+    @Json(name = "notes")
+    val notes: List<String> = listOf(),
+    @Json(name = "params")
+    val params: List<String> = listOf(),
+    @Json(name = "story")
+    val story: Any? = null
+)
