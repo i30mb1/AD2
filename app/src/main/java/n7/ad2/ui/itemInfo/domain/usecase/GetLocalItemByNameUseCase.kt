3152de639678d0a3fe3fdb7a20545319908e942e
i30mb1 <fate.i30mb1@gmail.com>
2020-10-17 14:15:52 +0300

    add GetLocalItemByName.kt

---
+++ app/src/main/java/n7/ad2/ui/itemInfo/domain/usecase/GetLocalItemByNameUseCase.kt
@@ -0,0 +1,18 @@
+package n7.ad2.ui.itemInfo.domain.usecase;
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
+import n7.ad2.data.source.local.db.AppDatabase
+import n7.ad2.data.source.local.model.LocalItem
+import javax.inject.Inject
+
+class GetLocalItemByNameUseCase @Inject constructor(
+    private val ioDispatcher: CoroutineDispatcher,
+    private val appDatabase: AppDatabase
+) {
+
+    @Suppress("BlockingMethodInNonBlockContext")
+    suspend operator fun invoke(itemName: String): LocalItem = withContext(ioDispatcher) {
+        appDatabase.itemsDao.getItem(itemName)
+    }
+}
