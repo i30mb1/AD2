10fe5f631e43e2fce09e9317444550446333f49c
i30mb1 <fate.i30mb1@gmail.com>
2020-10-30 18:19:24 +0300

    add method that remove Brackets from string

--- parseInfo/src/main/java/ad2/n7/parseinfo/Utils.kt
+++ parseInfo/src/main/java/ad2/n7/parseinfo/Utils.kt
@@ -4,16 +4,23 @@ import java.io.File
 import java.net.URL
 import javax.imageio.ImageIO
 
-val assets = System.getProperty("user.dir") + "\\app\\src\\main\\assets\\"
-const val HERO_FULL_PHOTO_TYPE = "png"
+internal val assets = System.getProperty("user.dir") + "\\app\\src\\main\\assets\\"
+internal const val HERO_FULL_PHOTO_TYPE = "png"
 
-fun saveImage(url: String, directoryInAssets: String, fileName: String) {
+internal fun String.removeBrackets(): String {
+    val regex = Regex(" \\(.+?\\)")
+    val matches = regex.containsMatchIn(this)
+    val result = if (matches) this.replace(regex, "") else this
+    return result.trim()
+}
+
+internal fun saveImage(url: String, directoryInAssets: String, fileName: String) {
     try {
         val bufferImageIO = ImageIO.read(URL(url))
         val file = File(assets + directoryInAssets + File.separator + "$fileName.$HERO_FULL_PHOTO_TYPE")
         file.mkdirs()
         ImageIO.write(bufferImageIO, HERO_FULL_PHOTO_TYPE, file)
-        println("image full saved")
+        println("image -$directoryInAssets saved")
     } catch (e: Exception) {
         println("image full not saved")
     }
