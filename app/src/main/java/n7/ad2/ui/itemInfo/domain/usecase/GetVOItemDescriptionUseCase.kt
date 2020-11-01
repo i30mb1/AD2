258c5082b1a786fe3a67f857ba0c41e211c7c4ae
i30mb1 <fate.i30mb1@gmail.com>
2020-11-01 13:50:08 +0300

    add recipe to VO

--- app/src/main/java/n7/ad2/ui/itemInfo/domain/usecase/GetVOItemDescriptionUseCase.kt
+++ app/src/main/java/n7/ad2/ui/itemInfo/domain/usecase/GetVOItemDescriptionUseCase.kt
@@ -1,25 +1,24 @@
-package n7.ad2.ui.itemInfo.domain.usecase;
+package n7.ad2.ui.itemInfo.domain.usecase
 
 import android.app.Application
 import android.text.SpannableString
-import android.widget.Button
-import android.widget.EditText
-import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
 import n7.ad2.R
 import n7.ad2.ui.heroInfo.domain.vo.VOBodyLine
+import n7.ad2.ui.heroInfo.domain.vo.VOBodyRecipe
 import n7.ad2.ui.heroInfo.domain.vo.VOBodySimple
 import n7.ad2.ui.heroInfo.domain.vo.VOBodyWithSeparator
 import n7.ad2.ui.heroInfo.domain.vo.VODescription
 import n7.ad2.ui.heroInfo.domain.vo.VOTitleSimple
+import n7.ad2.ui.itemInfo.domain.adapter.toVORecipe
 import n7.ad2.ui.itemInfo.domain.model.LocalItemDescription
 import n7.ad2.utils.extension.toStringListWithDash
 import javax.inject.Inject
 
 class GetVOItemDescriptionUseCase @Inject constructor(
     private val ioDispatcher: CoroutineDispatcher,
-    private val application: Application
+    private val application: Application,
 ) {
 
     suspend operator fun invoke(localItemDescription: LocalItemDescription): List<VODescription> = withContext(ioDispatcher) {
@@ -28,6 +27,11 @@ class GetVOItemDescriptionUseCase @Inject constructor(
             add(VOBodyLine(application.getString(R.string.cost, localItemDescription.cost)))
             add(VOBodyLine(application.getString(R.string.bought_from, localItemDescription.boughtFrom)))
             add(VOBodySimple(localItemDescription.description))
+
+            localItemDescription.consistFrom?.let {
+                add(VOTitleSimple(application.getString(R.string.recipe)))
+                add(VOBodyRecipe(it.map { itemName -> itemName.toVORecipe() }))
+            }
 
             localItemDescription.tips?.let {
                 add(VOTitleSimple(application.getString(R.string.tips)))
@@ -43,7 +47,6 @@ class GetVOItemDescriptionUseCase @Inject constructor(
                 add(VOTitleSimple(application.getString(R.string.additional_information)))
                 add(VOBodyWithSeparator(SpannableString(it.toStringListWithDash())))
             }
-
 
         }
     }
