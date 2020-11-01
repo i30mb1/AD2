40e748affd2a49051552ba6fe9f330d6ed435aa8
i30mb1 <fate.i30mb1@gmail.com>
2020-11-01 13:24:13 +0300

    upgrade RecyclerView to ListAdapter

--- app/src/main/java/n7/ad2/ui/itemInfo/RecipeImagesAdapter.kt
+++ app/src/main/java/n7/ad2/ui/itemInfo/RecipeImagesAdapter.kt
@@ -2,24 +2,18 @@ package n7.ad2.ui.itemInfo
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import n7.ad2.R
 import n7.ad2.databinding.ItemRecipeBinding
 import n7.ad2.ui.itemInfo.domain.vo.VORecipe
 
-class RecipeImagesAdapter : RecyclerView.Adapter<RecipeImagesAdapter.ViewHolder>() {
-
-    var list = emptyList<VORecipe>()
-        set(value) {
-            field = value
-            notifyDataSetChanged()
-        }
+class RecipeImagesAdapter : ListAdapter<VORecipe, RecipeImagesAdapter.ViewHolder>(DiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder = ViewHolder.from(parent)
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(list[position])
-
-    override fun getItemCount(): Int = list.size
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(getItem(position))
 
     override fun getItemViewType(position: Int): Int = R.layout.item_recipe
 
@@ -42,6 +36,11 @@ class RecipeImagesAdapter : RecyclerView.Adapter<RecipeImagesAdapter.ViewHolder>
             }
         }
 
+    }
+
+    private class DiffCallback : DiffUtil.ItemCallback<VORecipe>() {
+        override fun areItemsTheSame(oldItem: VORecipe, newItem: VORecipe): Boolean = oldItem.name == oldItem.name
+        override fun areContentsTheSame(oldItem: VORecipe, newItem: VORecipe): Boolean = true
     }
 
 }
