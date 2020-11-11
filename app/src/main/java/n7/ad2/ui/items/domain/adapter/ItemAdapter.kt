5048ff735e0881fe8aa4a3a2fc5c8fdc66516940
i30mb1 <fate.i30mb1@gmail.com>
2020-11-11 18:29:47 +0300

    fix converter from LocalItem to VOItem

--- app/src/main/java/n7/ad2/ui/items/domain/adapter/ItemAdapter.kt
+++ app/src/main/java/n7/ad2/ui/items/domain/adapter/ItemAdapter.kt
@@ -3,9 +3,8 @@ package n7.ad2.ui.items.domain.adapter
 import n7.ad2.data.source.local.model.LocalItem
 import n7.ad2.ui.items.domain.vo.VOItem
 
-fun LocalItem.toVO(): VOItem {
-    return VOItem().also {
-        it.name = name
-        it.image = "file:///android_asset/$assetsPath/full.png"
-    }
-}
+fun LocalItem.toVO(): VOItem = VOItem(
+    name,
+    "file:///android_asset/$assetsPath/full.png",
+    viewedByUser,
+)
