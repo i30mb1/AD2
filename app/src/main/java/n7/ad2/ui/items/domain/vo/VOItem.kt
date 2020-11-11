b851ee8d76eed35e716d6a84d5b61d0d914bcd1d
i30mb1 <fate.i30mb1@gmail.com>
2020-11-11 18:26:22 +0300

    add field 'viewedByUser' to VOItem model

--- app/src/main/java/n7/ad2/ui/items/domain/vo/VOItem.kt
+++ app/src/main/java/n7/ad2/ui/items/domain/vo/VOItem.kt
@@ -1,23 +1,7 @@
 package n7.ad2.ui.items.domain.vo
 
-import androidx.databinding.BaseObservable
-import androidx.databinding.Bindable
-import n7.ad2.BR
-
-class VOItem : BaseObservable() {
-
-    @get:Bindable
-    var name: String = ""
-        set(value) {
-            field = value
-            notifyPropertyChanged(BR.name)
-        }
-
-    @get:Bindable
-    var image: String = ""
-        set(value) {
-            field = value
-            notifyPropertyChanged(BR.image)
-        }
-
-}
+class VOItem(
+    val name: String,
+    val image: String,
+    val viewedByUser: Boolean,
+)
