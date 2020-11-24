9939e050874167c207c019098dccc2aa0c3594d5
i30mb1 <fate.i30mb1@gmail.com>
2020-11-24 20:26:31 +0300

    enhance Result.kt

--- app/src/main/java/n7/ad2/utils/Result.kt
+++ app/src/main/java/n7/ad2/utils/Result.kt
@@ -1,23 +1,22 @@
 package n7.ad2.utils
 
-sealed class Result<out T> {
-    class Failure<out T>(val exception: Throwable) : Result<T>()
-    class Success<out T>(val value: T) : Result<T>()
+sealed class Result<out T : Any> {
+    class Failure<out T : Any>(val exception: Throwable) : Result<T>()
+    class Success<out T : Any>(val value: T) : Result<T>()
 
     companion object {
-       fun <T> success(value: T): Success<T> = Success(value)
-        @JvmStatic
-       fun <T> failure(exception: Throwable): Failure<T> = Failure(exception)
+        fun <T : Any> success(value: T): Success<T> = Success(value)
+        fun failure(exception: Throwable): Failure<Nothing> = Failure(exception)
     }
 
 }
 
-inline fun <T> Result<T>.onSuccess(action: (value: T) -> Unit): Result<T> {
+inline fun <T : Any> Result<T>.onSuccess(action: (value: T) -> Unit): Result<T> {
     (this as? Result.Success)?.let { action(it.value) }
     return this
 }
 
-inline fun <T> Result<T>.onFailure(action: (exception: Throwable) -> Unit): Result<T> {
+inline fun <T : Any> Result<T>.onFailure(action: (exception: Throwable) -> Unit): Result<T> {
     (this as? Result.Failure)?.let { action(it.exception) }
     return this
 }
