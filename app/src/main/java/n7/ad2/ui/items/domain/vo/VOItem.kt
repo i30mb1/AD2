f2a6120b42e4148bf161fe308f62daf3a1ad39c8
i30mb1 <fate.i30mb1@gmail.com>
2020-11-24 20:25:46 +0300

    create variety of VOItem

--- app/src/main/java/n7/ad2/ui/items/domain/vo/VOItem.kt
+++ app/src/main/java/n7/ad2/ui/items/domain/vo/VOItem.kt
@@ -1,7 +1,5 @@
 package n7.ad2.ui.items.domain.vo
 
-class VOItem(
-    val name: String,
-    val image: String,
-    val viewedByUser: Boolean,
-)
+sealed class VOItem
+data class VOItemBody(val name: String, val image: String, val viewedByUser: Boolean) : VOItem()
+data class VOItemHeader(val title: String) : VOItem()
