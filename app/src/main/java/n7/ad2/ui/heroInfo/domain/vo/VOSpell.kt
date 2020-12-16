feebdeb6384f3b8889d305e00b2655dcb39cae32
i30mb1 <fate.i30mb1@gmail.com>
2020-12-16 16:41:48 +0300

    simplify VOSpell.kt class by removing unnecessary bindings

--- app/src/main/java/n7/ad2/ui/heroInfo/domain/vo/VOSpell.kt
+++ app/src/main/java/n7/ad2/ui/heroInfo/domain/vo/VOSpell.kt
@@ -4,34 +4,17 @@ import androidx.databinding.BaseObservable
 import androidx.databinding.Bindable
 import n7.ad2.BR
 
-class VOSpell : BaseObservable() {
-
-    @get:Bindable
-    var name: String = ""
-        set(value) {
-            field = value
-            notifyPropertyChanged(BR.name)
-        }
-
-    @get:Bindable
-    var image: String = ""
-        set(value) {
-            field = value
-            notifyPropertyChanged(BR.image)
-        }
+class VOSpell(
+    val name: String,
+    val urlSpellImage: String,
+    val voDescriptionList: List<VODescription>,
+) : BaseObservable() {
 
     @get:Bindable
     var selected: Boolean = false
         set(value) {
             field = value
             notifyPropertyChanged(BR.selected)
-        }
-
-    @get:Bindable
-    var listVODescriptions: List<VODescription> = emptyList()
-        set(value) {
-            field = value
-            notifyPropertyChanged(BR.listVODescriptions)
         }
 
 }
