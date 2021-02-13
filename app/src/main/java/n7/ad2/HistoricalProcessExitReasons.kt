7bc70747cbcf2d5e83cd5f9a71d110d344cae62c
i30mb1 <fate.i30mb1@gmail.com>
2021-02-13 14:36:23 +0300

    need to think how can i use it in my future log manager https://navczydev.medium.com/get-app-crash-info-using-gethistoricalprocessexitreasons-b62e8edd369f

--- app/src/main/java/n7/ad2/HistoricalProcessExitReasons.kt
+++ app/src/main/java/n7/ad2/HistoricalProcessExitReasons.kt
@@ -1,16 +1,20 @@
 package n7.ad2
 
 import android.app.ActivityManager
+import android.app.Application
+import android.content.Context
+import android.os.Build
+import androidx.annotation.RequiresApi
 
 class HistoricalProcessExitReasons {
 
-    fun get(activityManager: ActivityManager) {
-//        val reasonsList = activityManager.getHistoricalProcessExitReasons()
-//        for(info in resonsList) {
-//
-//        }
+    @RequiresApi(Build.VERSION_CODES.R)
+    fun get(application: Application) {
+        val activityManager = application.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        val reasonsList = activityManager.getHistoricalProcessExitReasons(null, 0, 10)
+        for (reason in reasonsList) {
 
+        }
     }
-
 
 }
