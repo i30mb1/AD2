c13c13c2e3f196a2ab0b4a546cb83ea4f0bf6768
i30mb1 <fate.i30mb1@gmail.com>
2021-02-28 16:16:58 +0300

    replace path on constant

--- app/src/main/java/n7/ad2/ui/heroes/domain/adapter/HeroAdapter.kt
+++ app/src/main/java/n7/ad2/ui/heroes/domain/adapter/HeroAdapter.kt
@@ -1,13 +1,13 @@
 package n7.ad2.ui.heroes.domain.adapter
 
+import n7.ad2.data.source.local.HeroRepository
 import n7.ad2.data.source.local.model.LocalHero
 import n7.ad2.ui.heroes.domain.vo.VOHero
-import java.io.File
 
-fun LocalHero.toVo() : VOHero {
+fun LocalHero.toVo(): VOHero {
     return VOHero().also {
         it.name = name
-        it.image = "file:///android_asset/$assetsPath/full.png"
+        it.image = HeroRepository.getFullUrlHeroImage(name)
         it.viewedByUser = viewedByUser
     }
 }
