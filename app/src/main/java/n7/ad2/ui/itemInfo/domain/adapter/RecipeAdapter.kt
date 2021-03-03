a079c0dfb629383efc692b111a907421ac833bd5
i30mb1 <fate.i30mb1@gmail.com>
2021-03-03 16:57:45 +0300

    fix voRecipe

--- app/src/main/java/n7/ad2/ui/itemInfo/domain/adapter/RecipeAdapter.kt
+++ app/src/main/java/n7/ad2/ui/itemInfo/domain/adapter/RecipeAdapter.kt
@@ -1,10 +1,9 @@
 package n7.ad2.ui.itemInfo.domain.adapter
 
+import n7.ad2.data.source.local.ItemRepository
 import n7.ad2.ui.itemInfo.domain.vo.VORecipe
 
-fun String.toVORecipe(): VORecipe {
-    return VORecipe(
-        "file:///android_asset/items/$this/full.png",
-        this
-    )
-}
+fun String.toVORecipe(): VORecipe = VORecipe(
+    ItemRepository.getFullUrlItemImage(this),
+    this,
+)
