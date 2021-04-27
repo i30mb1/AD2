fad827eeb6b8ba9a6c1390238d8f540fcb160d48
i30mb1 <fate.i30mb1@gmail.com>
2021-04-27 19:27:58 +0300

    add flowOn for GetHeroesFromRepositoryUseCase.kt

--- app/src/main/java/n7/ad2/ui/heroes/domain/usecase/GetHeroesFromRepositoryUseCase.kt
+++ app/src/main/java/n7/ad2/ui/heroes/domain/usecase/GetHeroesFromRepositoryUseCase.kt
@@ -2,7 +2,7 @@ package n7.ad2.ui.heroes.domain.usecase
 
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.flow.flowOn
 import n7.ad2.data.source.local.HeroRepository
 import n7.ad2.data.source.local.model.LocalHero
 import javax.inject.Inject
@@ -12,7 +12,6 @@ class GetHeroesFromRepositoryUseCase @Inject constructor(
     private val repository: HeroRepository,
 ) {
 
-    suspend operator fun invoke(filter: String): Flow<List<LocalHero>> = withContext(ioDispatcher) {
-        repository.getHeroes(filter)
-    }
+    operator fun invoke(filter: String): Flow<List<LocalHero>> = repository.getHeroes(filter).flowOn(ioDispatcher)
+
 }
