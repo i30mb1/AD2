938c3422715d7aab42305daec416ebae4af65a08
i30mb1 <fate.i30mb1@gmail.com>
2021-05-23 19:55:30 +0300

    inject usecases instead of db

--- app/src/main/java/n7/ad2/ui/items/ItemsViewModel.kt
+++ app/src/main/java/n7/ad2/ui/items/ItemsViewModel.kt
@@ -2,27 +2,42 @@ package n7.ad2.ui.items
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.switchMap
 import androidx.lifecycle.viewModelScope
-import androidx.paging.toLiveData
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
-import n7.ad2.data.source.local.db.AppDatabase
-import n7.ad2.ui.items.domain.adapter.toVOItemBody
+import n7.ad2.ui.items.domain.interactor.GetVOItemsInteractor
+import n7.ad2.ui.items.domain.usecase.FilterItemsUseCase
 import n7.ad2.ui.items.domain.usecase.UpdateItemViewedByUserFieldUseCase
+import n7.ad2.ui.items.domain.vo.VOItem
 import javax.inject.Inject
 
 class ItemsViewModel @Inject constructor(
     application: Application,
-    appDatabase: AppDatabase,
+    getVOItemsInteractor: GetVOItemsInteractor,
+    private val filterItemsUseCase: FilterItemsUseCase,
     private val updateItemViewedByUserFieldUseCase: UpdateItemViewedByUserFieldUseCase,
 ) : AndroidViewModel(application) {
 
-    private val itemsDao = appDatabase.itemsDao
-    private val itemsFilter = MutableLiveData("")
-    val itemsPagedList = itemsFilter.switchMap {
-        itemsDao.getItemsFilter(it).map { it.toVOItemBody() }.toLiveData(20)
+    private val allItems: MutableStateFlow<List<VOItem>> = MutableStateFlow(emptyList())
+    private val _filteredItems: MutableStateFlow<List<VOItem>> = MutableStateFlow(emptyList())
+    val filteredItems: StateFlow<List<VOItem>> = _filteredItems.asStateFlow()
+
+    init {
+        getVOItemsInteractor()
+            .onEach(allItems::emit)
+            .onEach(_filteredItems::emit)
+            .launchIn(viewModelScope)
     }
+
+    fun filterItems(filter: String) = allItems
+        .map { list -> filterItemsUseCase(list, filter) }
+        .onEach(_filteredItems::emit)
+        .launchIn(viewModelScope)
 
     fun updateViewedByUserFieldForItem(name: String) {
         viewModelScope.launch {
