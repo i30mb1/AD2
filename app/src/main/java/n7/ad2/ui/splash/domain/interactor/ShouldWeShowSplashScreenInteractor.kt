634453fa4ce4526abf93cf2333dfc317f483d68e
i30mb1 <fate.i30mb1@gmail.com>
2021-05-23 19:24:24 +0300

    do not change context in ShouldWeShowSplashScreenInteractor.kt

--- app/src/main/java/n7/ad2/ui/splash/domain/interactor/ShouldWeShowSplashScreenInteractor.kt
+++ app/src/main/java/n7/ad2/ui/splash/domain/interactor/ShouldWeShowSplashScreenInteractor.kt
@@ -1,14 +1,11 @@
 package n7.ad2.ui.splash.domain.interactor
 
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.withContext
 import n7.ad2.ui.splash.domain.usecase.GetCurrentDateInYearUseCase
 import n7.ad2.ui.splash.domain.usecase.GetLastDayShownSplashScreenUseCase
 import n7.ad2.ui.splash.domain.usecase.SaveLastDayShownSplashScreenUseCase
 import javax.inject.Inject
 
 class ShouldWeShowSplashScreenInteractor @Inject constructor(
-    private val ioDispatcher: CoroutineDispatcher,
     private val getCurrentDateUseCase: GetCurrentDateInYearUseCase,
     private val getLastDayShownSplashScreenUseCase: GetLastDayShownSplashScreenUseCase,
     private val saveLastDayShownSplashScreenUseCase: SaveLastDayShownSplashScreenUseCase,
@@ -18,13 +15,13 @@ class ShouldWeShowSplashScreenInteractor @Inject constructor(
         private const val LAST_DAY_SHOWN_SPLASH_SCREEN_KEY = "LAST_DAY_SHOWN_SPLASH_SCREEN_KEY"
     }
 
-    suspend operator fun invoke(): Boolean = withContext(ioDispatcher) {
+    suspend operator fun invoke(): Boolean {
         val currentDayInYear = getCurrentDateUseCase()
         val lastDayShownSplashScreen = getLastDayShownSplashScreenUseCase(LAST_DAY_SHOWN_SPLASH_SCREEN_KEY)
         if (currentDayInYear < lastDayShownSplashScreen) {
             saveLastDayShownSplashScreenUseCase(LAST_DAY_SHOWN_SPLASH_SCREEN_KEY, currentDayInYear)
-            return@withContext true
+            return true
         }
-        return@withContext false
+        return false
     }
 }
