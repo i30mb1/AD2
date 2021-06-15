b37e8ea304f0c506d8b6b7f4095be46dc0de2423
i30mb1 <fate.i30mb1@gmail.com>
2021-06-15 19:07:30 +0300

    add SaveLastDayHeroGuidesLoadedUseCase.kt

---
+++ app/src/main/java/n7/ad2/ui/heroGuide/domain/usecase/SaveLastDayHeroGuidesLoadedUseCase.kt
@@ -0,0 +1,19 @@
+package n7.ad2.ui.heroGuide.domain.usecase
+
+import android.content.SharedPreferences
+import androidx.core.content.edit
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class SaveLastDayHeroGuidesLoadedUseCase @Inject constructor(
+    private val ioDispatcher: CoroutineDispatcher,
+    private val preferences: SharedPreferences,
+) {
+
+    suspend operator fun invoke(key: String, day: Int) = withContext(ioDispatcher) {
+        preferences.edit(commit = true) {
+            putInt(key, day)
+        }
+    }
+}
