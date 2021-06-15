98f6d0650edec0887d712392d1ea74bd1a3b66c3
i30mb1 <fate.i30mb1@gmail.com>
2021-06-15 19:07:44 +0300

    add ShouldWeLoadNewHeroGuidesInteractor.kt

---
+++ app/src/main/java/n7/ad2/ui/heroGuide/domain/interactor/ShouldWeLoadNewHeroGuidesInteractor.kt
@@ -0,0 +1,32 @@
+package n7.ad2.ui.heroGuide.domain.interactor
+
+import n7.ad2.AD2Logger
+import n7.ad2.ui.heroGuide.domain.usecase.GetLastDayHeroGuidesLoadedUseCase
+import n7.ad2.ui.heroGuide.domain.usecase.SaveLastDayHeroGuidesLoadedUseCase
+import n7.ad2.ui.splash.domain.usecase.GetCurrentDateInYearUseCase
+import javax.inject.Inject
+
+class ShouldWeLoadNewHeroGuidesInteractor @Inject constructor(
+    private val getCurrentDateUseCase: GetCurrentDateInYearUseCase,
+    private val getLastDayHeroGuidesLoadedUseCase: GetLastDayHeroGuidesLoadedUseCase,
+    private val saveLastDayHeroGuidesLoadedUseCase: SaveLastDayHeroGuidesLoadedUseCase,
+    private val logger: AD2Logger,
+) {
+
+    companion object {
+        private fun getKey(heroName: String) = "LAST_DAY_HERO_GUIDES_LOADED_$heroName"
+    }
+
+    suspend operator fun invoke(heroName: String): Boolean {
+        val currentDayInYear = getCurrentDateUseCase()
+        val lastDayHeroGuidesLoadedUseCase = getLastDayHeroGuidesLoadedUseCase(getKey(heroName))
+        return if (lastDayHeroGuidesLoadedUseCase < currentDayInYear) {
+            saveLastDayHeroGuidesLoadedUseCase(getKey(heroName), currentDayInYear)
+            logger.log("we should load new guide for $heroName")
+            true
+        } else {
+            logger.log("we have already downloaded guide for $heroName")
+            false
+        }
+    }
+}
