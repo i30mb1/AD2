2a870460f481b5c125910fd3817720af90e660d9
i30mb1 <fate.i30mb1@gmail.com>
2021-07-21 19:23:21 +0300

    convert StreamsViewModel to kotlin

--- app/src/main/java/n7/ad2/ui/streams/StreamsViewModel.kt
+++ app/src/main/java/n7/ad2/ui/streams/StreamsViewModel.kt
@@ -1,25 +1,33 @@
-package n7.ad2.ui.streams;
+package n7.ad2.ui.streams
 
-import android.app.Application;
-import androidx.lifecycle.AndroidViewModel;
-import androidx.lifecycle.LiveData;
-import androidx.paging.LivePagedListBuilder;
-import androidx.paging.PagedList;
-import androidx.databinding.ObservableBoolean;
-import androidx.annotation.NonNull;
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
+import n7.ad2.data.source.remote.StreamPagingSource
+import n7.ad2.data.source.remote.model.Stream
+import javax.inject.Inject
 
+class StreamsViewModel @Inject constructor(
+    private val application: Application,
+    private val streamPagingSource: StreamPagingSource,
+) : ViewModel() {
 
-public class StreamsViewModel extends AndroidViewModel {
-
-    Application application;
-    private final StreamsStorage streamsStorage;
-    public ObservableBoolean isLoading = new ObservableBoolean();
-
-    public StreamsViewModel(@NonNull Application application) {
-        super(application);
-        this.application = application;
-
-        streamsStorage = new StreamsStorage(application, isLoading);
+    fun getStreams(): Flow<PagingData<Stream>> {
+        return Pager(
+            config = PagingConfig(
+                pageSize = 33,
+                enablePlaceholders = false,
+            ),
+            pagingSourceFactory = { streamPagingSource }
+        )
+            .flow
+            .stateIn(viewModelScope, SharingStarted.Lazily, PagingData.empty())
     }
 
 }
