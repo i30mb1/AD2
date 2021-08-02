2027fd3c6d16225c75909e6ea614b06a65c88967
i30mb1 <fate.i30mb1@gmail.com>
2021-08-02 21:31:28 +0300

    update exoPlayer version

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -42,7 +42,7 @@ object Lib {
     const val firebaseAnal = "com.google.firebase:firebase-analytics:17.3.0"
     const val playCore = "com.google.android.play:core:1.8.0" // auto update + rateMe
     const val playCoreKtx = "com.google.android.play:core-ktx:1.8.1"
-    const val exoPlayer = "com.google.android.exoplayer:exoplayer-core:2.11.4"
+    const val exoPlayer = "com.google.android.exoplayer:exoplayer-core:2.14.2"
     const val coreKtx = "androidx.core:core-ktx:1.6.0-alpha02" // Write more concise, idiomatic Kotlin code.
     const val jsonSimple = "com.googlecode.json-simple:json-simple:1.1.1"
     const val springAnimation = "androidx.dynamicanimation:dynamicanimation:1.0.0"
