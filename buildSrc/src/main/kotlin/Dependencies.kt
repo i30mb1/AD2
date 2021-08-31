bd6235dfc2c1ce83d3f3c2de0eba6e5bc759b00a
i30mb1 <fate.i30mb1@gmail.com>
2021-08-31 19:00:26 +0300

    add dataStory dependency to project

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -21,9 +21,11 @@ object Versions {
     const val lifecycle = "2.4.0-alpha01"
     const val retrofit = "2.7.1"
     const val coroutines = "1.5.1"
+    const val dataStore = "1.0.0"
 }
 
 object Lib {
+    const val dataStore = "androidx.datastore:datastore:${Versions.dataStore}"
     const val recyclerView = "androidx.recyclerview:recyclerview:1.2.0-beta02"
     const val kotlinReflection = "org.jetbrains.kotlin:kotlin-reflect:${Versions.kotlin}"
     const val ticker = "com.robinhood.ticker:ticker:1.2.2"
@@ -141,6 +143,7 @@ fun DependencyHandler.addBaseDependencies() {
     api(Lib.springAnimationKtx)
     api(Lib.exoPlayer)
     api(Lib.recyclerView)
+    api(Lib.dataStore)
 
     api(Lib.coroutinesLifecycle)
     api(Lib.coroutinesLivedata)
