afed05c98a3cfe27898da8da603dc0b5c6cf1255
i30mb1 <fate.i30mb1@gmail.com>
2021-10-25 10:47:25 +0300

    add utils to extract colors from theme

---
+++ app/src/main/java/n7/ad2/utils/MaterialColors.kt
@@ -0,0 +1,6 @@
+package n7.ad2.utils
+
+import android.view.View
+import com.google.android.material.color.MaterialColors
+
+fun getColor(view: View, color: Int) = MaterialColors.getColor(view, color)
