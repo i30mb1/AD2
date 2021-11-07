a8be34b505d659f6d3426c34923434bbc4b16c47
i30mb1 <fate.i30mb1@gmail.com>
2021-11-07 20:23:25 +0300

    combine adapter with use case

--- app/src/main/java/n7/ad2/ui/splash/domain/usecase/ConvertAssetsHeroListToLocalHeroListUseCase.kt
+++ app/src/main/java/n7/ad2/ui/splash/domain/usecase/ConvertAssetsHeroListToLocalHeroListUseCase.kt
@@ -1,15 +1,18 @@
 package n7.ad2.ui.splash.domain.usecase
 
-import n7.ad2.ui.splash.domain.model.AssetsHero
 import n7.ad2.data.source.local.model.LocalHero
-import n7.ad2.ui.splash.domain.adapter.toLocalHero
+import n7.ad2.ui.splash.domain.model.AssetsHero
 import javax.inject.Inject
 
 class ConvertAssetsHeroListToLocalHeroListUseCase @Inject constructor() {
 
     operator fun invoke(assetsHeroList: List<AssetsHero>): List<LocalHero> {
         return assetsHeroList.map {
-            it.toLocalHero()
+            LocalHero(
+                name = it.name,
+                mainAttr = it.mainAttr,
+                viewedByUser = false,
+            )
         }
     }
 }
--- app/src/main/java/n7/ad2/ui/splash/domain/usecase/ConvertAssetsItemListToLocalItemListUseCase.kt
+++ app/src/main/java/n7/ad2/ui/splash/domain/usecase/ConvertAssetsItemListToLocalItemListUseCase.kt
@@ -1,7 +1,6 @@
 package n7.ad2.ui.splash.domain.usecase
 
 import n7.ad2.data.source.local.model.LocalItem
-import n7.ad2.ui.splash.domain.adapter.toLocalItem
 import n7.ad2.ui.splash.domain.model.AssetsItem
 import javax.inject.Inject
 
@@ -9,7 +8,11 @@ class ConvertAssetsItemListToLocalItemListUseCase @Inject constructor() {
 
     operator fun invoke(assetsItemList: List<AssetsItem>): List<LocalItem> {
         return assetsItemList.map {
-            it.toLocalItem()
+            LocalItem(
+                name = it.name,
+                type = it.section,
+                viewedByUser = false,
+            )
         }
     }
 
