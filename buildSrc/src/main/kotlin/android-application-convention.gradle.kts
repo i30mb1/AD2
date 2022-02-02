2aaa1c18dc10cc829df2b12c5927937705304bce
i30mb1 <fate.i30mb1@gmail.com>
2022-02-02 19:21:07 +0300

    disable debug variant to speed up build phase

--- buildSrc/src/main/kotlin/android-application-convention.gradle.kts
+++ buildSrc/src/main/kotlin/android-application-convention.gradle.kts
@@ -1,6 +1,16 @@
+import com.android.build.gradle.AppExtension
+
 plugins {
     id("com.android.application")
     id("base-android-convention")
     id("kotlin-android")
     id("base-kotlin-convention")
+}
+
+configure<AppExtension> {
+    buildTypes {
+        getByName("debug") {
+            setMatchingFallbacks("release")
+        }
+    }
 }
--- buildSrc/src/main/kotlin/android-library-convention.gradle.kts
+++ buildSrc/src/main/kotlin/android-library-convention.gradle.kts
@@ -1,6 +1,12 @@
+import com.android.build.gradle.LibraryExtension
+
 plugins {
     id("com.android.library")
     id("base-android-convention")
     id("kotlin-android")
     id("base-kotlin-convention")
+}
+
+configure<LibraryExtension> {
+    variantFilter { if (name == "debug") ignore = true }
 }
