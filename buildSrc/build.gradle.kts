a61b2eef2316bdde9c5f6ba4fd36727f5808bb5a
i30mb1 <fate.i30mb1@gmail.com>
2022-03-20 00:52:15 +0300

    revert gradle version

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -17,6 +17,6 @@ repositories {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:7.1.2")
+    implementation("com.android.tools.build:gradle:7.0.4")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.30")
 }
