e95c8e48e4c0663b91014e4a500347e24fb4b0b1
i30mb1 <fate.i30mb1@gmail.com>
2022-04-03 13:52:06 +0300

    add few dependencies to convention dependencies

--- build-dependency/src/main/kotlin/convention.dependencies.settings.gradle.kts
+++ build-dependency/src/main/kotlin/convention.dependencies.settings.gradle.kts
@@ -1,5 +1,13 @@
 dependencyResolutionManagement {
     repositories {
+        google()
         mavenCentral()
+        exclusiveContent {
+            forRepository { google() }
+            filter {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google\\.android\\..*")
+            }
+        }
     }
 }
--- build-dependency/src/main/kotlin/convention.plugins.settings.gradle.kts
+++ build-dependency/src/main/kotlin/convention.plugins.settings.gradle.kts
@@ -1,9 +1,27 @@
 pluginManagement {
-    repositories { maven { setUrl("https://plugins.gradle.org/m2/") } }
+    repositories {
+        exclusiveContent {
+            forRepository {
+                gradlePluginPortal() // kotlin-dsl, kotlin, jvm, kapt
+            }
+            filter {
+                includeGroup("org.gradle.kotlin")
+            }
+        }
+
+    }
+
     resolutionStrategy {
         eachPlugin {
-            if (requested.id.namespace == "org.gradle.kotlin") useVersion("2.1.7")
-            else if (requested.id.namespace == "org.jetbrains.kotlin") useVersion("1.6.10")
+            val pluginId = requested.id.id
+            val namespace = requested.id.namespace
+            when {
+                namespace == "org.gradle.kotlin" -> useVersion("2.1.7")
+                namespace == "org.jetbrains.kotlin" -> useVersion("1.6.10") // useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.30")
+                namespace == "com.android" -> useVersion("7.1.2") // useModule("com.android.tools.build:gradle:7.1.2")
+                pluginId == "androidx.benchmark" -> useModule("androidx.benchmark:benchmark-gradle-plugin:1.1.0-beta05")
+            }
         }
     }
+
 }
