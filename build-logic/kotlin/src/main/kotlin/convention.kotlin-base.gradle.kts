b9f6ce779a4caa5914714fc9bb44a99d741e07e7
i30mb1 <fate.i30mb1@gmail.com>
2022-08-31 23:15:37 +0300

    disable test warning for new API

--- build-logic/kotlin/src/main/kotlin/convention.kotlin-base.gradle.kts
+++ build-logic/kotlin/src/main/kotlin/convention.kotlin-base.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 tasks.withType<KotlinCompile>().configureEach {
     kotlinOptions {
-        freeCompilerArgs = freeCompilerArgs + listOf("-Xopt-in=kotlin.RequiresOptIn")
+        freeCompilerArgs = freeCompilerArgs + listOf("-Xopt-in=kotlin.RequiresOptIn", "-Xopt-in=kotlinx.coroutines.ExperimentalCoroutinesApi")
         jvmTarget = "11"
     }
 }
