5b26d6acf29023ad8dd33b7d35784665f6de4b62
i30mb1 <fate.i30mb1@gmail.com>
2022-08-31 23:15:05 +0300

    small optimizations

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,10 +10,3 @@ plugins {
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
-
-val printFromGradleProperties by tasks.registering {
-    group = "n7"
-    doLast {
-        println("Hello ${project.property("key")}")
-    }
-}
