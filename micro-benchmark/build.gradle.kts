b5a54cffb5cbee40287527a3daaaea35b0e3a29a
i30mb1 <fate.i30mb1@gmail.com>
2022-08-31 23:15:02 +0300

    update libs

--- micro-benchmark/build.gradle.kts
+++ micro-benchmark/build.gradle.kts
@@ -11,8 +11,7 @@ android {
 }
 
 dependencies {
-    androidTestImplementation("androidx.test:runner:1.4.0")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.benchmark:benchmark-junit4:1.1.0-beta05")
+    androidTestImplementation(libs.testRunner)
+    androidTestImplementation(libs.testJunit)
+    androidTestImplementation(libs.benchmarkJunit)
 }
