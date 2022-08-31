ab77d24789624ce6ab24decf82ecdf209c9b2678
i30mb1 <fate.i30mb1@gmail.com>
2022-08-31 23:15:07 +0300

    update AGP

--- build-logic/kotlin/build.gradle.kts
+++ build-logic/kotlin/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:7.1.2")
+    implementation("com.android.tools.build:gradle:7.2.0")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
     implementation(project(":extensions"))
 }
