cbee62fbde5e4b6fa0e1de67da195e6418adb0ef
i30mb1 <fate.i30mb1@gmail.com>
2022-08-31 23:14:53 +0300

    add more info about user device

--- app/src/main/kotlin/n7/ad2/init/SystemInfoInitializer.kt
+++ app/src/main/kotlin/n7/ad2/init/SystemInfoInitializer.kt
@@ -2,6 +2,7 @@ package n7.ad2.init
 
 import android.app.ActivityManager
 import android.app.Application
+import android.os.Build
 import android.provider.Settings
 import androidx.core.content.getSystemService
 import n7.ad2.AppInformation
@@ -11,10 +12,14 @@ class SystemInfoInitializer : Initializer {
 
     override fun init(app: Application, logger: AD2Logger, appInformation: AppInformation) {
         val androidID = Settings.Secure.getString(app.contentResolver, Settings.Secure.ANDROID_ID)
+        val androidVersion = Build.VERSION.SDK_INT
+        val cpuCount = Runtime.getRuntime().availableProcessors()
         logger.log("SYSTEM INFO:")
         logger.log("------")
         logger.log("IS_DEBUG = ${appInformation.isDebug}")
         logger.log("ANDROID_ID = $androidID")
+        logger.log("ANDROID_VERSION = $androidVersion")
+        logger.log("CPU_COUNT = $cpuCount")
         logger.log("memory = ${getMemoryInfo(app)}")
         logger.log("------")
     }
