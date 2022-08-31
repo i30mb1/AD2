6539b5d3a465280245a6244d2cc2228737f1fa15
i30mb1 <fate.i30mb1@gmail.com>
2022-08-31 23:15:03 +0300

    delete redundant block

--- build-logic/kotlin/build.gradle.kts
+++ build-logic/kotlin/build.gradle.kts
@@ -2,11 +2,6 @@ plugins {
     `kotlin-dsl`
 }
 
-repositories {
-    mavenCentral()
-    google()
-}
-
 dependencies {
     implementation("com.android.tools.build:gradle:7.1.2")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
