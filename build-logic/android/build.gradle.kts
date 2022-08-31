a1b7a0b97b820e64ee7a480452c8e7d55dc799a9
i30mb1 <fate.i30mb1@gmail.com>
2022-08-31 23:14:43 +0300

    update libs versions

--- build-logic/android/build.gradle.kts
+++ build-logic/android/build.gradle.kts
@@ -4,5 +4,5 @@ plugins {
 
 dependencies {
     implementation("com.android.tools.build:gradle:7.1.2")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.20")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,7 +20,7 @@ pluginManagement {
             val namespace = requested.id.namespace
             when {
                 namespace == "org.gradle.kotlin" -> useVersion("2.1.7")
-                namespace == "org.jetbrains.kotlin" -> useVersion("1.6.10") // useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.30")
+                namespace == "org.jetbrains.kotlin" -> useVersion("1.6.20") // useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.30")
                 namespace == "com.android" -> useVersion("7.1.2") // useModule("com.android.tools.build:gradle:7.1.2")
                 pluginId == "androidx.benchmark" -> useModule("androidx.benchmark:benchmark-gradle-plugin:1.1.0-beta05")
             }
