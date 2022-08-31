eba94b5fd0967158a4b091f92bb9b2d2fe9600a2
i30mb1 <fate.i30mb1@gmail.com>
2022-08-31 23:15:29 +0300

    ad ui test

---
+++ app/src/androidTest/kotlin/Test.kt
@@ -0,0 +1,25 @@
+import androidx.lifecycle.Lifecycle
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import n7.ad2.ui.MainActivity
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class Test {
+
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun appLaunchesSuccessfully() {
+        activityRule.scenario.moveToState(Lifecycle.State.RESUMED)
+        Espresso.onView(ViewMatchers.withId(R.id.tv_fps))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+}
