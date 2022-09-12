22c1fe6fc44e4de7737a817f4784dd9d6fd8c6b2
i30mb1 <fate.i30mb1@gmail.com>
2022-09-12 22:00:17 +0300

    add insets for NewsFragment.kt

--- feature/news/src/main/kotlin/ad2/n7/news/internal/NewsFragment.kt
+++ feature/news/src/main/kotlin/ad2/n7/news/internal/NewsFragment.kt
@@ -10,10 +10,13 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -21,10 +24,13 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -34,8 +40,10 @@ import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.items
 import kotlinx.coroutines.launch
+import n7.ad2.android.DrawerPercentListener
 import n7.ad2.android.findDependencies
 import n7.ad2.ktx.viewModel
+import n7.ad2.logger.Logger
 import n7.ad2.ui.ComposeView
 import n7.ad2.ui.compose.AppTheme
 import javax.inject.Inject
@@ -47,6 +55,8 @@ internal class NewsFragment : Fragment() {
     }
 
     @Inject lateinit var newsViewModelFactory: NewsViewModel.Factory
+
+    @Inject lateinit var logger: Logger
 
     private val viewModel: NewsViewModel by viewModel { newsViewModelFactory.create() }
 
@@ -60,7 +70,7 @@ internal class NewsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
-        return ComposeView { NewsScreen(viewModel) }
+        return ComposeView { NewsScreen(viewModel, parentFragment as DrawerPercentListener) }
     }
 
 }
@@ -68,11 +78,14 @@ internal class NewsFragment : Fragment() {
 @Composable
 internal fun NewsScreen(
     viewModel: NewsViewModel,
+    drawerPercentListener: DrawerPercentListener,
     modifier: Modifier = Modifier,
 ) {
     val scope = rememberCoroutineScope()
     val state = rememberLazyListState()
 
+    val insetsTop = WindowInsets.statusBars.asPaddingValues().calculateTopPadding()
+    var drawerPercent by remember { mutableStateOf(0f) }
     val showScrollToTopButton by remember { derivedStateOf { state.firstVisibleItemIndex > 0 } /* recomposition only triggered when the state of calculation changed*/ }
     val news: LazyPagingItems<NewsVO> = viewModel.news.collectAsLazyPagingItems()
     Box {
@@ -81,7 +94,7 @@ internal fun NewsScreen(
             state = state,
             verticalArrangement = Arrangement.spacedBy(8.dp),
             contentPadding = PaddingValues(
-                top = 30.dp,
+                top = 4.dp + insetsTop * drawerPercent,
                 start = 4.dp,
                 end = 4.dp,
             )
@@ -104,7 +117,10 @@ internal fun NewsScreen(
             }
         }
     }
-
+    DisposableEffect(key1 = Unit, effect = {
+        drawerPercentListener.setDrawerPercentListener { percent -> drawerPercent = percent }
+        onDispose { drawerPercentListener.setDrawerPercentListener(null) }
+    })
 }
 
 @Composable
@@ -129,5 +145,4 @@ internal fun NewsItem(item: NewsVO?) {
             color = AppTheme.color.textColor,
         )
     }
-
 }
