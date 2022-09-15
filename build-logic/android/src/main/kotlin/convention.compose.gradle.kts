707cde6bed541209bd7656efabb7f896439ea5e3
i30mb1 <fate.i30mb1@gmail.com>
2022-09-15 20:28:57 +0300

    bump compose dependencies

--- build-logic/android/src/main/kotlin/convention.compose.gradle.kts
+++ build-logic/android/src/main/kotlin/convention.compose.gradle.kts
@@ -6,8 +6,7 @@ import gradle.kotlin.dsl.accessors._6076749d6351b9fdce8c5a61387fb22f.implementat
 configure<BaseExtension> {
     buildFeatures.compose = true
     composeOptions {
-//        kotlinCompilerExtensionVersion = libs.versions.compose.get()
-        kotlinCompilerExtensionVersion = "1.3.0"
+        kotlinCompilerExtensionVersion = libs.versions.composeCompiler.get()
     }
 }
 
