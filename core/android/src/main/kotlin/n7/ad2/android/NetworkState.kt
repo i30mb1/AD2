bd0a8383e4971d973f5f40a31bb887127c4a310c
i30mb1 <fate.i30mb1@gmail.com>
2022-11-20 00:31:35 +0300

    fix NetworkState class

--- core/android/src/main/kotlin/n7/ad2/android/NetworkState.kt
+++ core/android/src/main/kotlin/n7/ad2/android/NetworkState.kt
@@ -12,7 +12,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
 
 class NetworkState(
-    private val context: Context,
+    context: Context,
 ) {
 
     sealed class Status {
@@ -20,18 +20,19 @@ class NetworkState(
         object Available : Status()
     }
 
+    private val connectivityManager: ConnectivityManager = context.getSystemService()!!
     @SuppressLint("MissingPermission") val networkState: Flow<Status> = callbackFlow {
         val networkCallback = object : ConnectivityManager.NetworkCallback() {
             override fun onAvailable(network: Network) {
-                trySend(Status.Unavailable)
+                trySend(Status.Available)
             }
 
             override fun onUnavailable() {
-                trySend(Status.Available)
+                trySend(Status.Unavailable)
             }
 
             override fun onLost(network: Network) {
-                trySend(Status.Available)
+                trySend(Status.Unavailable)
             }
         }
         val request = NetworkRequest.Builder()
@@ -40,7 +41,5 @@ class NetworkState(
         connectivityManager.registerNetworkCallback(request, networkCallback)
         awaitClose { connectivityManager.unregisterNetworkCallback(networkCallback) }
     }
-    private val connectivityManager: ConnectivityManager = context.getSystemService()!!
-
 
 }
