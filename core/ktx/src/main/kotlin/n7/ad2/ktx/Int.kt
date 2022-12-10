7d367c8f233e4b9d7719ad81425fbd958db94664
i30mb1 <fate.i30mb1@gmail.com>
2022-12-10 23:49:16 +0300

    add space background for KillCreepScreen.kt game

--- core/ktx/src/main/kotlin/n7/ad2/ktx/Int.kt
+++ core/ktx/src/main/kotlin/n7/ad2/ktx/Int.kt
@@ -9,5 +9,8 @@ import kotlin.math.roundToInt
 val Int.dpToPx: Int
     get() = (this * Resources.getSystem().displayMetrics.density.roundToInt())
 
+val Float.dpToPx: Float
+    get() = (this * Resources.getSystem().displayMetrics.density.roundToInt())
+
 val Float.spToPx: Float
     get() = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, this, Resources.getSystem().displayMetrics)
