40f63b290141aac30e6208236d36a3a5c7a13b7a
i30mb1 <fate.i30mb1@gmail.com>
2022-12-12 16:18:17 +0300

    extract settings compose function into separate file

--- feature/settings/src/main/kotlin/ad2/n7/settings/internal/SettingsFragment.kt
+++ feature/settings/src/main/kotlin/ad2/n7/settings/internal/SettingsFragment.kt
@@ -1,6 +1,7 @@
 package ad2.n7.settings.internal
 
 import ad2.n7.settings.R
+import ad2.n7.settings.internal.compose.SettingsScreen
 import android.content.Intent
 import android.net.Uri
 import android.os.Build
@@ -8,26 +9,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Star
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import com.google.android.play.core.review.ReviewManagerFactory
 import n7.ad2.ui.ComposeView
-import n7.ad2.ui.compose.AppTheme
 
 class SettingsFragment : Fragment() {
 
@@ -54,43 +38,6 @@ class SettingsFragment : Fragment() {
 
                 }
             }
-        }
-    }
-
-    @Preview
-    @Composable
-    fun SettingsScreen(
-        onAppReviewButtonClicked: () -> Unit = {},
-        onTellFriendsButtonClicked: () -> Unit = {},
-    ) {
-        Column {
-            SimpleItem(stringResource(R.string.setting_review_app), onAppReviewButtonClicked)
-            SimpleItem(stringResource(R.string.setting_tell_friend_about_this_app), onTellFriendsButtonClicked)
-        }
-    }
-
-    @Composable
-    fun SimpleItem(
-        name: String,
-        onSimpleItemClicked: () -> Unit,
-    ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(AppTheme.color.surface)
-                .clickable { onSimpleItemClicked() }
-                .padding(12.dp, 8.dp),
-        ) {
-            Text(
-                text = name,
-                modifier = Modifier
-                    .align(Alignment.CenterVertically)
-                    .weight(1f),
-                color = AppTheme.color.textColor
-            )
-            Icon(
-                Icons.Default.Star, null
-            )
         }
     }
 
