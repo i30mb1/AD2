b86ef45eb44f1b4ec3ec0f70250afb3ce983a994
i30mb1 <fate.i30mb1@gmail.com>
2022-12-27 18:31:42 +0300

    fix locale

--- app/src/main/kotlin/n7/ad2/AD2AppInformation.kt
+++ app/src/main/kotlin/n7/ad2/AD2AppInformation.kt
@@ -7,7 +7,7 @@ class AD2AppInformation @Inject constructor(
 ) : AppInformation {
     override val isDebug = BuildConfig.DEBUG
     override val appLocale: AppLocale = when (val value = res.getString(n7.ad2.commonandroid.R.string.locale)) {
-        "ru" -> AppLocale.Russian
+        "RU" -> AppLocale.Russian
         else -> AppLocale.English
     }
     override val appVersion = BuildConfig.VERSION_NAME
--- core/common-jvm/src/main/kotlin/n7/ad2/AppLocale.kt
+++ core/common-jvm/src/main/kotlin/n7/ad2/AppLocale.kt
@@ -1,6 +1,6 @@
 package n7.ad2
 
 sealed class AppLocale(val value: String) {
-    object English : AppLocale("ru")
-    object Russian : AppLocale("en")
+    object English : AppLocale("en")
+    object Russian : AppLocale("ru")
 }
