223cb2944cc39d8afe1891468ebaa9ad8ca6b647
yauhen.shuvahin <yauhen.shuvahin@adevinta.com>
2023-02-05 20:49:31 +0300

    rename folder

---
+++ build-logic/build-settings/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    `kotlin-dsl`
+    `maven-publish`
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            group = "n7"
+            artifactId = "convention"
+            version = "1.0"
+            from(components["java"])
+        }
+
+    }
+}
---
+++ build-logic/build-settings/settings.gradle.kts
@@ -0,0 +1,14 @@
+rootProject.name = "build-settings"
+
+dependencyResolutionManagement {
+    repositories {
+        gradlePluginPortal() // kotlin-stdlib-jdk8
+    }
+}
+
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal() // kotlin-dsl
+    }
+}
