b9b3fc6afed3a710815337b2841e9780f80a9af1
i30mb1 <fate.i30mb1@gmail.com>
2023-02-15 10:36:34 +0300

    update gradle

--- build-logic/detekt/build.gradle.kts
+++ build-logic/detekt/build.gradle.kts
@@ -14,14 +14,3 @@ dependencies {
     testImplementation(libs.detektTest)
     testImplementation(libs.bundles.test)
 }
-
-// using to avoid compile Kotlin with old version
-// https://handstandsam.com/2022/04/13/using-the-kotlin-dsl-gradle-plugin-forces-kotlin-1-4-compatibility/
-afterEvaluate {
-    tasks.withType<KotlinCompile>().configureEach {
-        kotlinOptions {
-            apiVersion = "1.7"
-            languageVersion = "1.7"
-        }
-    }
-}
