00db4a3c7b48a30570cbc3c0cb65e831bf64f67c
i30mb1 <fate.i30mb1@gmail.com>
2023-02-15 21:35:17 +0300

    try to writ animation for Favorite icon

--- feature/news/src/main/kotlin/n7/ad2/news/internal/screen/list/compose/NewsItem.kt
+++ feature/news/src/main/kotlin/n7/ad2/news/internal/screen/list/compose/NewsItem.kt
@@ -2,17 +2,33 @@
 
 package n7.ad2.news.internal.screen.list.compose
 
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animate
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Icon
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -47,21 +63,76 @@ internal fun NewsItem(
         elevation = 4.dp,
         onClick = { onNewsClicked(item.id) },
     ) {
-        AsyncImage(
-            model = item.image.origin,
-            contentDescription = item.title,
-            contentScale = ContentScale.Crop,
-            placeholder = painterResource(id = R.drawable.widht_placeholder),
+        Box {
+            AsyncImage(
+                model = item.image.origin,
+                contentDescription = item.title,
+                contentScale = ContentScale.Crop,
+                placeholder = painterResource(R.drawable.widht_placeholder),
+            )
+            Text(
+                modifier = Modifier
+                    .background(Color(0x4D000000))
+                    .fillMaxSize()
+                    .padding(start = 16.dp, end = 16.dp, top = 32.dp, bottom = 32.dp),
+                textAlign = TextAlign.Center,
+                text = item.title,
+                style = AppTheme.style.H5,
+                color = Color.White,
+            )
+            var isLiked by remember { mutableStateOf(false) }
+            FavoriteIcon(
+                isLiked,
+                { isLiked = !isLiked },
+                modifier = Modifier.align(Alignment.TopEnd)
+            )
+        }
+    }
+}
+
+@Composable
+private fun FavoriteIcon(
+    isLiked: Boolean,
+    onLikeClicked: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    var filledLikeScale by remember { mutableStateOf(0f) }
+    var showEmptyLike by remember { mutableStateOf(false) }
+    Box(
+        modifier = modifier
+            .clickable {
+                onLikeClicked()
+            }
+            .padding(8.dp),
+    ) {
+        if (isLiked) Icon(
+            imageVector = Icons.Default.Favorite,
+            contentDescription = null,
+        ) else Icon(
+            imageVector = Icons.Default.FavoriteBorder,
+            contentDescription = null,
         )
-        Text(
-            modifier = Modifier
-                .background(Color(0x4D000000))
-                .fillMaxSize()
-                .padding(start = 16.dp, end = 16.dp, top = 32.dp, bottom = 32.dp),
-            textAlign = TextAlign.Center,
-            text = item.title,
-            style = AppTheme.style.H5,
-            color = Color.White,
+        if (filledLikeScale > 0f) Icon(
+            modifier = Modifier.scale(filledLikeScale),
+            imageVector = Icons.Default.Favorite,
+            contentDescription = null,
         )
+    }
+
+    LaunchedEffect(isLiked) {
+        if (isLiked) {
+            animate(
+                0f,
+                1f,
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioHighBouncy,
+                ),
+            ) { value, _ ->
+                filledLikeScale = value
+                if (value >= 1f) {
+                    showEmptyLike = false
+                }
+            }
+        }
     }
 }
