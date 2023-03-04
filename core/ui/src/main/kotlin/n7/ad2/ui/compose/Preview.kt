d538978f8fe49bd23bd37b1e58f3d0aa78137b76
i30mb1 <fate.i30mb1@gmail.com>
2023-03-04 14:58:39 +0300

    add ThemePreview annotation that is shorthand and more convenient

---
+++ core/ui/src/main/kotlin/n7/ad2/ui/compose/Preview.kt
@@ -0,0 +1,8 @@
+package n7.ad2.ui.compose
+
+import android.content.res.Configuration
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(name = "Light")
+@Preview(name = "Dark", uiMode = Configuration.UI_MODE_NIGHT_YES)
+annotation class ThemePreview
