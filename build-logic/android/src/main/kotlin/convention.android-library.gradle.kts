4100d2e1cdaebf1af52a154517bed31926db945c
i30mb1 <fate.i30mb1@gmail.com>
2023-07-22 14:34:17 +0300

    master: turn on debug buildType for UI modules

--- build-logic/android/src/main/kotlin/convention.android-library.gradle.kts
+++ build-logic/android/src/main/kotlin/convention.android-library.gradle.kts
@@ -8,7 +8,7 @@ android {
     namespace = "$applicationID.${project.path.replace("-", "").replace(":", ".").drop(1)}"
     androidComponents {
         beforeVariants { variantBuilder ->
-            if (variantBuilder.buildType == "debug") variantBuilder.enable = false
+            if (variantBuilder.buildType == "debug" && project.name != "ui") variantBuilder.enable = false
         }
     }
 }
