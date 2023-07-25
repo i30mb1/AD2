43c8dda094bd7d2b0edd564755e9f809f746bdad
i30mb1 <fate.i30mb1@gmail.com>
2023-07-25 20:09:06 +0300

    master: add FindViewAction to find necessary view using espresso

---
+++ feature/heroes/ui/src/androidTest/kotlin/n7/ad2/heroes/domain/ui/FindViewAction.kt
@@ -0,0 +1,24 @@
+package n7.ad2.heroes.domain.ui
+
+import android.view.View
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
+import androidx.test.espresso.matcher.ViewMatchers
+import org.hamcrest.Matcher
+
+class FindViewAction : ViewAction {
+
+    lateinit var targetView: View
+
+    override fun getDescription(): String {
+        return "extract view from activity"
+    }
+
+    override fun getConstraints(): Matcher<View> {
+        return ViewMatchers.isAssignableFrom(View::class.java)
+    }
+
+    override fun perform(uiController: UiController?, view: View?) {
+        targetView = requireNotNull(view)
+    }
+}
