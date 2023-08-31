51c43fc85f303a5bcdb35927205728bf216f5682
i30mb1 <fate.i30mb1@gmail.com>
2023-08-31 10:10:27 +0300

    master: delete old gradle plugin that do not support Gradle 8

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     id("bump-version-plugin")
     id("measure-build-plugin")
     id("convention.detekt")
-//    id("com.dorongold.task-tree") version "2.1.0" // adds a 'taskTree' task that prints task dependency tree
 //    id("com.osacky.doctor") version "0.7.3"
 //    id("com.autonomousapps.dependency-analysis") version "1.0.0-rc02"
     // https://arrow-kt.io/docs/meta/analysis/
