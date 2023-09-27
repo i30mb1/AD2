aa33ba4404a55fe8541ae2e47bd76b3b180a37a7
i30mb1 <fate.i30mb1@gmail.com>
2023-09-27 21:02:48 +0300

    master: add constructor inject into Application

--- app/src/main/kotlin/n7/ad2/AD2AppComponentFactory.kt
+++ app/src/main/kotlin/n7/ad2/AD2AppComponentFactory.kt
@@ -2,16 +2,18 @@ package n7.ad2
 
 import android.app.AppComponentFactory
 import android.app.Application
+import n7.ad2.di.DaggerApplicationComponent
 import n7.ad2.ui.MyApplication
 
 class AD2AppComponentFactory : AppComponentFactory() {
+
+    private val applicationFactory = DaggerApplicationComponent.factory()
 
     override fun instantiateApplication(
         classLoader: ClassLoader,
         className: String,
     ): Application = when (className) {
-        MyApplication::class.java.name -> MyApplication()
+        MyApplication::class.java.name -> MyApplication(applicationFactory)
         else -> super.instantiateApplication(classLoader, className)
     }
-
 }
--- app/src/main/kotlin/n7/ad2/ui/MyApplication.kt
+++ app/src/main/kotlin/n7/ad2/ui/MyApplication.kt
@@ -1,24 +1,25 @@
 package n7.ad2.ui
 
 import android.app.Application
+import javax.inject.Inject
 import n7.ad2.AppInformation
 import n7.ad2.android.DependenciesMap
 import n7.ad2.android.HasDependencies
+import n7.ad2.app.logger.Logger
 import n7.ad2.di.ApplicationComponent
-import n7.ad2.di.DaggerApplicationComponent
 import n7.ad2.di.DaggerComponentProvider
 import n7.ad2.init.Initializer
 import n7.ad2.ktx.lazyUnsafe
-import n7.ad2.app.logger.Logger
-import javax.inject.Inject
 
 // https://medium.com/bumble-tech/how-we-achieved-a-6x-reduction-of-anrs-part-2-fixing-anrs-24fedf9a973f
 // “Code never lies, comments sometimes do” — Ron Jeffries
-class MyApplication : Application(), DaggerComponentProvider, HasDependencies {
+class MyApplication(
+    factory: ApplicationComponent.Factory,
+) : Application(), DaggerComponentProvider, HasDependencies {
 
     @Inject override lateinit var dependenciesMap: DependenciesMap
 
-    override val component: ApplicationComponent by lazyUnsafe { DaggerApplicationComponent.factory().create(this) }
+    override val component: ApplicationComponent by lazyUnsafe { factory.create(this) }
 
     override fun onCreate() {
         component.inject(this)
@@ -29,5 +30,4 @@ class MyApplication : Application(), DaggerComponentProvider, HasDependencies {
     fun init(initializers: Set<@JvmSuppressWildcards Initializer>, logger: Logger, appInformation: AppInformation) {
         initializers.forEach { initializer -> initializer.init(this, logger, appInformation) }
     }
-
 }
