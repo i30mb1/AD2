c9d9eea8619e1a452996f0ac1809d86584e4ba89
i30mb1 <fate.i30mb1@gmail.com>
2023-11-02 09:11:39 +0300

    master: add notes for coroutines

--- core/coroutines/src/main/kotlin/n7/ad2/coroutines/extension/CoroutineScope.kt
+++ core/coroutines/src/main/kotlin/n7/ad2/coroutines/extension/CoroutineScope.kt
@@ -1,17 +1,42 @@
 package n7.ad2.coroutines.extension
 
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
-import kotlin.coroutines.CoroutineContext
-import kotlin.coroutines.EmptyCoroutineContext
 
+/**
+ * Launches coroutines with proper exception and completion
+ *
+ * @param onError callback that receive error wrapped in Exception to get detailed information about where exception arrised
+ * @param onComplete callback called when coroutines job complete
+ */
 inline fun CoroutineScope.launchSave(
     context: CoroutineContext = EmptyCoroutineContext,
+    crossinline onComplete: () -> Unit = { },
     crossinline onError: (Throwable) -> Unit,
     crossinline block: suspend () -> Unit,
 ): Job {
-    val exceptionHandler = CoroutineExceptionHandler { _, throwable -> onError(Exception(throwable)) }
-    return launch(exceptionHandler + context) { block() }
+    val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
+        onError(Exception(throwable))
+    }
+    return launch(exceptionHandler + context) {
+        block()
+    }.apply {
+        invokeOnCompletion {
+            onComplete()
+        }
+    }
+}
+
+/**
+ * Checks if exception is CancellationException and if tue rethrow it
+ */
+fun Throwable.checkCancellation() {
+    if (this is CancellationException) {
+        throw this
+    }
 }
