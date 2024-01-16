ecdbe9c0504ac78ec290c54c44a1eaf1e4b5bafd
i30mb1 <fate.i30mb1@gmail.com>
2024-01-16 21:59:42 +0300

    master: fix git hook

--- build-logic/build-settings/src/main/kotlin/n7/plugins/GitHooksPlugin.kt
+++ build-logic/build-settings/src/main/kotlin/n7/plugins/GitHooksPlugin.kt
@@ -3,13 +3,16 @@ package n7.plugins
 import org.gradle.api.Plugin
 import org.gradle.api.initialization.Settings
 
-class GitHooksPlugin: Plugin<Settings> {
+class GitHooksPlugin : Plugin<Settings> {
 
     override fun apply(target: Settings) = with(target) {
-       val output = providers.exec {
-            commandLine("git", "config", "core.hooksPath", ".githooks")
+        try {
+            val output = providers.exec {
+                commandLine("git", "config", "core.hooksPath", ".githooks")
+            }
+            println("AD2: setup core.hooksPath: exitValue=${output.result.orNull?.exitValue}")
+        } catch (e: Exception) {
+            println("Error when trying to set git-hooks")
         }
-        println("setup core.hooksPath: exitValue=${output.result.orNull?.exitValue}")
     }
-
 }
