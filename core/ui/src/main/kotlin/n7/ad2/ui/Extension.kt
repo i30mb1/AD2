ab7ce938f0268772edc29b9c6826260fb2e642e6
i30mb1 <fate.i30mb1@gmail.com>
2024-02-03 15:05:37 +0300

    master: add extension for view

--- core/ui/src/main/kotlin/n7/ad2/ui/Extension.kt
+++ core/ui/src/main/kotlin/n7/ad2/ui/Extension.kt
@@ -1,7 +1,17 @@
 package n7.ad2.ui
 
+import android.content.res.ColorStateList
+import android.content.res.TypedArray
+import android.util.AttributeSet
 import android.view.View
 import android.view.View.OnAttachStateChangeListener
+import android.widget.ImageView
+import androidx.annotation.AttrRes
+import androidx.annotation.ColorInt
+import androidx.annotation.ColorRes
+import androidx.annotation.StyleRes
+import androidx.annotation.StyleableRes
+import androidx.core.content.res.ResourcesCompat
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
 import kotlinx.coroutines.CoroutineScope
@@ -32,4 +42,35 @@ fun View.whileAttachedOnce(
         }
     })
     if (isAttachedToWindow) launchWork()
+}
+
+/**
+ * Usage:
+ * withTypedArray(attrs, R.styleable.SquareCheckBoxView) {
+ *             text = this.getString(R.styleable.SquareCheckBoxView_check_text) ?: ""
+ *             borderColor = this.getColor(R.styleable.SquareCheckBoxView_border_color, resolveColor(R.color.default_border_color))
+ *         }
+ */
+fun View.withTypedArray(
+    set: AttributeSet?,
+    @StyleableRes attrs: IntArray,
+    @AttrRes defStyleAttr: Int = 0,
+    @StyleRes defStyleRes: Int = 0,
+    action: TypedArray.() -> Unit,
+) {
+    val typedArray = context.theme.obtainStyledAttributes(
+        set, attrs, defStyleAttr, defStyleRes
+    )
+    action(typedArray)
+    typedArray.recycle()
+}
+
+@ColorInt
+fun View.resolveColor(@ColorRes colorRes: Int): Int =
+    ResourcesCompat.getColor(this.context.resources, colorRes, null)
+
+fun ImageView.setTint(@ColorRes color: Int) {
+    this.imageTintList = ColorStateList.valueOf(
+        resolveColor(color)
+    )
 }
