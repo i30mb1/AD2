80d5356a095c31347520d3a16a22459123adfc82
i30mb1 <fate.i30mb1@gmail.com>
2024-05-08 10:04:57 +0300

    master: add test for StateFlow

---
+++ core/coroutines/src/test/kotlin/StateFlowTest.kt
@@ -0,0 +1,13 @@
+import kotlinx.coroutines.debug.junit4.CoroutinesTimeout
+import org.junit.Rule
+import org.junit.Test
+
+class StateFlowTest {
+
+    @get:Rule val timeout = CoroutinesTimeout.seconds(5)
+
+    @Test
+    fun `simple state flow`() {
+
+    }
+}
