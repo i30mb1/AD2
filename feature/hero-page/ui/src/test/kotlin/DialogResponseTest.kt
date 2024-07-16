63a196882baf6972236713cf9c4a1ef2b68e4f13
i30mb1 <fate.i30mb1@gmail.com>
2024-07-16 16:15:58 +0300

    master: fix snapshot test that run with robolectric on windows

--- feature/hero-page/ui/src/test/kotlin/DialogResponseTest.kt
+++ feature/hero-page/ui/src/test/kotlin/DialogResponseTest.kt
@@ -1,21 +1,23 @@
+import android.os.Build
 import androidx.core.os.bundleOf
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.lifecycle.Lifecycle
-import androidx.test.espresso.Espresso
-import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.espresso.screenshot.captureToBitmap
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.runner.screenshot.Screenshot
+import n7.ad2.core.ui.R
 import n7.ad2.hero.page.internal.responses.DialogResponse
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.annotation.Config
+import org.robolectric.annotation.GraphicsMode
 
 @RunWith(AndroidJUnit4::class)
 // GraphicsMode is not supported in windows https://github.com/robolectric/robolectric/issues/8312
-// @GraphicsMode(GraphicsMode.Mode.NATIVE)
+@GraphicsMode(GraphicsMode.Mode.NATIVE)
 @Config(
     manifest = Config.NONE,
+    sdk = [Build.VERSION_CODES.TIRAMISU],
+    qualifiers = "ru-rRU-w412dp-h732dp-night-navhidden",
 )
 class DialogResponseTest {
 
@@ -24,15 +26,13 @@ class DialogResponseTest {
         val fragmentArgs = bundleOf("selectedListItem" to 0)
         val scenario = launchFragmentInContainer<DialogResponse>(
             fragmentArgs = fragmentArgs,
-            themeResId = com.google.android.material.R.style.Base_ThemeOverlay_AppCompat_Dark
+            themeResId = R.style.AD2Theme,
         )
         scenario.onFragment {
             scenario.moveToState(Lifecycle.State.RESUMED)
         }
 
         val bitmap = Screenshot.capture().bitmap
-        val bitmap2 = Espresso.onView(ViewMatchers.isRoot()).captureToBitmap()
-        println("")
     }
 }
 
