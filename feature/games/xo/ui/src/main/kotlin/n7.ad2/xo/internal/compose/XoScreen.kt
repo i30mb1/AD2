79bcf8e232370c9d00339f6b04292aa8b409293f
Shuvagin <e.shuvagin@tinkoff.ru>
2024-08-13 12:13:13 +0300

    temporary disable info

--- feature/games/xo/ui/src/main/kotlin/n7.ad2/xo/internal/compose/XoScreen.kt
+++ feature/games/xo/ui/src/main/kotlin/n7.ad2/xo/internal/compose/XoScreen.kt
@@ -2,6 +2,7 @@ package n7.ad2.xo.internal.compose
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -19,7 +20,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import n7.ad2.app.logger.model.AppLog
 import n7.ad2.feature.games.xo.ui.R
-import n7.ad2.logger.ui.Logger
 import n7.ad2.ui.compose.AppTheme
 import n7.ad2.xo.internal.XoUIState
 import n7.ad2.xo.internal.compose.model.ServerUI
@@ -48,59 +48,64 @@ internal fun XoScreen(
         modifier = modifier
             .safeDrawingPadding(),
     ) {
-        Column(
-            modifier = Modifier
-                .align(Alignment.TopEnd)
-                .fillMaxWidth()
-                .padding(start = 8.dp, end = 8.dp)
+//        NetworkInfo(state)
+//        Logger(
+//            logs = state.logs,
+//            modifier = Modifier
+//                .align(Alignment.BottomEnd)
+//                .safeDrawingPadding(),
+//        )
+        when {
+            state.isGameStarted -> GameScreen(messages = state.messages, events)
+            else -> StaringScreen(state, events)
+        }
+    }
+}
+
+@Composable
+private fun BoxScope.NetworkInfo(state: XoUIState) {
+    Column(
+        modifier = Modifier.Companion
+            .align(Alignment.TopEnd)
+            .fillMaxWidth()
+            .padding(start = 8.dp, end = 8.dp)
+    ) {
+        Row(
+            modifier = Modifier.align(Alignment.End),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalAlignment = Alignment.CenterVertically,
         ) {
-            Row(
-                modifier = Modifier.align(Alignment.End),
-                horizontalArrangement = Arrangement.spacedBy(8.dp),
-                verticalAlignment = Alignment.CenterVertically,
-            ) {
-                val icon = if (state.deviceIP.isEmpty()) R.drawable.wifi_off else R.drawable.wifi
-                Icon(
-                    modifier = Modifier.size(16.dp),
-                    imageVector = ImageVector.vectorResource(icon),
-                    tint = AppTheme.color.textSecondaryColor,
-                    contentDescription = null,
+            val icon = if (state.deviceIP.isEmpty()) R.drawable.wifi_off else R.drawable.wifi
+            Icon(
+                modifier = Modifier.size(16.dp),
+                imageVector = ImageVector.vectorResource(icon),
+                tint = AppTheme.color.textSecondaryColor,
+                contentDescription = null,
+            )
+            Text(
+                text = state.deviceIP,
+                style = AppTheme.style.body,
+                color = AppTheme.color.textSecondaryColor,
+            )
+        }
+        state.server?.let { server ->
+            Row(modifier = Modifier.align(Alignment.End)) {
+                Text(
+                    text = "Host for ",
+                    style = AppTheme.style.body,
+                    color = AppTheme.color.textSecondaryColor,
                 )
                 Text(
-                    text = state.deviceIP,
+                    text = server.ip,
+                    style = AppTheme.style.body,
+                    color = AppTheme.color.textSecondaryColor,
+                )
+                Text(
+                    text = ":${server.port}",
                     style = AppTheme.style.body,
                     color = AppTheme.color.textSecondaryColor,
                 )
             }
-            state.server?.let { server ->
-                Row(modifier = Modifier.align(Alignment.End)) {
-                    Text(
-                        text = "Host for ",
-                        style = AppTheme.style.body,
-                        color = AppTheme.color.textSecondaryColor,
-                    )
-                    Text(
-                        text = server.ip,
-                        style = AppTheme.style.body,
-                        color = AppTheme.color.textSecondaryColor,
-                    )
-                    Text(
-                        text = ":${server.port}",
-                        style = AppTheme.style.body,
-                        color = AppTheme.color.textSecondaryColor,
-                    )
-                }
-            }
-        }
-        Logger(
-            logs = state.logs,
-            modifier = Modifier
-                .align(Alignment.BottomEnd)
-                .safeDrawingPadding(),
-        )
-        when {
-            state.isGameStarted -> GameScreen(messages = state.messages, events)
-            else -> StaringScreen(state, events)
         }
     }
 }
